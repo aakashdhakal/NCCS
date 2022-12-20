be8133310bf617d5c1c86005194bd6a3ac3d2de8
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2022-12-20 11:20:16 +0545

    Add files via upload

---
+++ C++/++ operator overloading.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Time{
+	int hour;
+	int min;
+	int sec;
+	public:
+	/*----	Time(int h,int s, int m){
+			h = hour;
+			m = min;
+			s = sec;
+		}    ----*/                                                                                                                         
+		
+		Time operator ++(){
+			sec++;
+			min += sec/60;
+			hour += min/60;
+			sec %= 60;
+			min %= 60;
+			return *this;
+		}
+		void inTime(){
+			cout<<"Please enter time in hour:minute:second: ";
+			cin>>hour>>min>>sec;
+		}	
+		
+		void outTime(){
+			cout<<"The time is "<<hour<<" : "<<min<<" : "<<sec<<endl;
+		}
+};
+
+int main(){
+	Time t1;
+	t1.inTime();
+	cout<<"Before increment"<<endl;
+	t1.outTime();
+	cout<<"After increment"<<endl;
+	(++t1).outTime();
+}
