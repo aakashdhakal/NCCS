e561295c7adcd53d23cc908f53c1ff9efc6c198c
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2022-12-31 18:26:08 +0545

    Add files via upload

--- C++/Project/aakashtravels.cpp
+++ C++/Project/aakashtravels.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n = 0;
+int np = 0;
 
 class Bus{
 	string destination;
@@ -9,20 +10,92 @@ class Bus{
 	int busNumber;
 	string arrivalTime;
 	string departureTime;
+	int fare;
+	int seats[8][4];
 	public:
 		
 	void inputInfo();
 	void showInfo();
 	void removeInfo();
+	void seatInfo();
+	friend class Passenger;
 
 
 	
 } b[10];
 
+class Passenger{
+	string name;
+	int seatNumber;
+	int ticketNumber;
+	public:
+		void bookTicket();
+		void showTicket();
+		friend class Bus;
+		
+}p[1000];
+
+
+void Bus::seatInfo(){
+	cout<<"------------------------------------------------"<<endl;
+	int q=0;
+
+	for (int i=0;i<8;i++){
+		for(int j=0;j<4;j++){
+			cout.width(10);
+			if(seats[i][j] != 0){
+			cout<<++q<<". BOOKED";
+		}
+		else{
+			cout<<++q<<". Empty";
+		}
+		}
+		cout<<endl;
+	}
+	cout<<"------------------------------------------------"<<endl;
+}
+
+
+void roleSelect();
+void emptyList(){
+		cout<<"------------------------------------------------"<<endl;
+		cout<<"The list is empty"<<endl;
+		cout<<"------------------------------------------------"<<endl;
+}
+
+void passenger(){
+	cout<<"Select an action\n 1. See Available Buses\n 2. Book a ticket\n 3. View your ticket\n 4. Change User Role\n 5. Exit"<<endl;
+		int option;
+		cin>>option;
+	
+			switch(option){
+		
+				case 1: b[0].showInfo();
+						break;
+		
+				case 2: p[0].bookTicket();
+						break;
+				
+				case 3: 
+						//p[0].showTicket();
+						break;
+						
+				case 4: roleSelect();
+						break;
+						
+				case 5: exit(0);
+						break;
+				default:  cout<<"Error! Please enter numbers 1 or 2 or 3"<<endl;
+							passenger();
+							break;
+	}
+	passenger();
+}
+
 void busAdmin(){
 		
-
-		cout<<"Select an action\n 1. Add a bus\n 2. Delete bus\n 3. View Available Buses\n 4. Exit"<<endl;
+	cout<<"------------------------------------------------"<<endl;
+		cout<<"Select an action\n 1. Add a bus\n 2. Delete bus\n 3. View Available Buses\n 4. Change User Role\n 5. Exit"<<endl;
 		int option;
 		cin>>option;
 	
@@ -38,16 +111,23 @@ void busAdmin(){
 						b[0].showInfo();
 						break;
 						
-				case 4: exit(0);
+				case 4: roleSelect();
+						break;
+
+				case 5: exit(0);
 						break;
 				
 				default:  cout<<"Error! Please enter numbers 1 or 2 or 3"<<endl;
 							busAdmin();
 							break;
 	}
+
+		cout<<"------------------------------------------------"<<endl;
+			busAdmin();
 }
 
 	void roleSelect(){
+			cout<<"------------------------------------------------"<<endl;
 		cout<<"Please mention your role:\n 1. Admin \n 2. Passenger"<<endl;
 		int option;
 		cin>>option;
@@ -57,19 +137,91 @@ void busAdmin(){
 				case 1: busAdmin();
 				break;
 		
-				case 2: cout<<"test"<<endl;
+				case 2: passenger();
 				break;
 		
 				default: cout<<"Error! Please enter numbers 1 or 2"<<endl;
 						roleSelect();
 						break;
 	}
-	
+		cout<<"------------------------------------------------"<<endl;
 }
 
-	void Bus::inputInfo(){
 
-		cout<<"Enter the bus number: ";
+void Passenger::bookTicket(){
+	string dest;
+	int bnum,temp = 0,i;
+	cout<<"Please enter the desired destination: ";
+	cin>>dest;
+	
+	for(int i=0 ;i<n; i++){
+		
+		if(dest == b[i].destination){
+			cout<<"------------------------------------------------"<<endl;
+			cout<<"Bus number: "<<b[i].busNumber<<endl;
+			cout<<"Driver's Name: "<<b[i].driverName<<endl;
+			cout<<"From: "<<b[i].origin<<endl;
+			cout<<"To: "<<b[i].destination<<endl;
+			cout<<"Arrival Time: "<<b[i].arrivalTime<<endl;
+			cout<<"Departure Time: "<<b[i].departureTime<<endl;
+			cout<<"Fare: Rs "<<b[i].fare<<endl;
+			cout<<"------------------------------------------------"<<endl;
+		}
+		
+		else{
+			temp++;
+		}
+	}
+		if(temp == n){
+			cout<<"ERROR! No buses are available for the given destination"<<endl;
+			passenger();
+		}
+		
+		cout<<"Enter bus number: ";
+		cin>>bnum;
+		
+		for(i = 0; i<n; i++){
+			
+			if(bnum == b[i].busNumber){
+				break;	
+			}
+		}
+		
+
+			b[i].seatInfo();
+		snum:
+		cout<<"Enter seat number: ";
+		cin>>p[np].seatNumber;
+		if(b[i].seats[p[np].seatNumber/4][(p[np].seatNumber%4)-1] != 0){
+			cout<<"ERROR! The seat is already reserved"<<endl;
+			goto snum;
+			
+		}
+		
+		cout<<"Enter passenger's name: ";
+		cin>>p[np].name;
+		p[np].ticketNumber = 1000 + (rand() % 1000);
+		b[i].seats[p[np].seatNumber/4][(p[np].seatNumber%4)-1] = p[np].ticketNumber;
+		cout<<"Your ticket number is: "<<p[np].ticketNumber<<endl;
+		np++;
+		cout<<"(NOTE: Please note down your ticket number as it is required to get details about your seat)"<<endl;
+		cout<<"Your seat is reserved successfully"<<endl;
+		system("pause");
+}
+
+
+
+
+
+
+
+
+
+
+
+		void Bus::inputInfo(){
+			
+cout<<"Enter the bus number: ";
 		cin>>b[n].busNumber;
 		
 		for (int i=0;i<n;i++){
@@ -91,15 +243,12 @@ void busAdmin(){
 		cout<<"Bus added successfully"<<endl;
 		n++;
 		system("pause");
-		busAdmin();
 		
 	}
 	
 	void Bus::showInfo(){
 if(n<=0){
-		cout<<"------------------------------------------------"<<endl;
-	cout<<"The list is empty"<<endl;
-		cout<<"------------------------------------------------"<<endl;
+	emptyList();
 }
 else{
 for(int i=0;i<n;i++){
@@ -110,10 +259,10 @@ for(int i=0;i<n;i++){
 	cout<<"To: "<<b[i].destination<<endl;
 	cout<<"Arrival Time: "<<b[i].arrivalTime<<endl;
 	cout<<"Departure Time: "<<b[i].departureTime<<endl;
+	cout<<"Fare: Rs "<<b[i].fare<<endl;
 	cout<<"------------------------------------------------"<<endl;
 }
 }
-		busAdmin();
 }
 
 void Bus::removeInfo() {
@@ -122,38 +271,32 @@ void Bus::removeInfo() {
 
 	  
 if(n<=0){
-		cout<<"------------------------------------------------"<<endl;
-	cout<<"The list is empty"<<endl;
-		cout<<"------------------------------------------------"<<endl;
+	emptyList();
 }
 	  else{
-	  		  cout<<"Enter the bus number: ";
-	  cin >> number;
-	  for (int i = 0; i < n; i++) {
-	    if (b[i].busNumber == number) {
-	     		 b[i] = b[i + 1];
-	    	n--;
-	    	cout<<"The bus is removed from the list!"<<endl;
+	  	cout<<"------------------------------------------------"<<endl;
+	  	cout<<"Enter the bus number: ";
+	  	cin >> number;
+	  		for (int i = 0; i < n; i++) {
+	    		if (b[i].busNumber == number) {
+	     			 b[i] = b[i + 1];
+	    				n--;
+	    			cout<<"The bus is removed from the list!"<<endl;
+	    			busAdmin();
 	  }
 	  
-	  else{
-	  	cout<<"Error! Bus not found in the list"<<endl;
-	  	removeInfo();
-	  }
+	  
 }
-
+	  	cout<<"Error! Bus not found in the list"<<endl;
+	cout<<"------------------------------------------------"<<endl;
+	removeInfo();
 	}
-		  busAdmin();
 }
 	
-class Passenger{
-	string name;
-	int seatNumber;
-	int ticketNumber;
-	public:
-};
+
 
 int main(){
+	srand(time(0));
   	cout<<"E-Ticket"<<endl;
   	roleSelect();
 	return 0;
