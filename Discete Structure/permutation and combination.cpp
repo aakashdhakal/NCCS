fb6a513d5cad3896ad96f6daa9e22e29e0b7599f
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-02-12 13:54:54 +0545

    Add files via upload

---
+++ Discete Structure/permutation and combination.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+ long fact(int n)
+{
+    if(n==0)
+    {
+        return 1;
+        
+    }
+    else
+    {
+        return n*fact(n-1);    
+    }
+}
+int main()
+{
+    
+ long n,r ,r1;
+cout<<"Enter value of  N and C \n ";
+cin>>n>>r;
+r1=fact(n)/(fact(n-r)*fact(r));
+cout<<"Number of possible combination is "<<r1;
+    
+    return 0;
+}
