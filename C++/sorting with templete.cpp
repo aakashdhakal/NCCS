e5c54025487c220c4a6b60e87a1a8e0995f05430
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-05-03 22:24:54 +0545

    Add files via upload

--- C++/sorting with templete.cpp
+++ C++/sorting with templete.cpp
@@ -22,17 +22,27 @@ void show(T arr[], int n){
 }
 
 int main() {
-    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr1[] = { 64, 34, 25, 12, 22, 11, 90 };
+    float arr2[] = {4.4, 1.2, 0.1, 5.12, 2.4, 9.3, 6.1};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
 
     cout << "Before sorting: ";
-    show(arr,n);
-    cout << std::endl;
+    show(arr1,n1);
+    cout << endl;
+    show(arr2,n2);
+    cout << endl;
 
-    bubbleSort(arr, n);
+    bubbleSort(arr1, n1);
+    bubbleSort(arr2, n2);
 
     cout << "After sorting: ";
- show(arr,n);
+ 	show(arr1,n1);
+ 	cout << endl;
+ 	show(arr2,n2);
+ 	cout << endl;
+ 	
+ 	
 
     return 0;
 }
