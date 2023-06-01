36bba80a5cd4c0168819302a34cdb98b60f1130b
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-01 12:12:00 +0545

    Update Fixed Point Method.c

--- Third Semester/Numerical Method/Fixed Point Method.c
+++ Third Semester/Numerical Method/Fixed Point Method.c
@@ -1,35 +1,52 @@
-#include <stdio.h>
-#include <conio.h>
-#include <math.h>
+/* Program: Finding real roots of nonlinear
+   equation using Fixed Point Iteration
+   Author: CodeSansar
+   Date: November 18, 2018 */
+/* Header Files */
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
 
-float g(float x)
-{
-    float y;
-    y = 2.0 - x * x;
-    return y;
-}
+/* Define function f(x) which
+   is to be solved */
+#define   f(x)   cos(x)-3*x+1
+/* Write f(x) as x = g(x) and
+   define g(x) here */
+#define   g(x)   (1+cos(x))/3
 
 int main()
 {
-    float x0, x, error, E = 0.00001;
-
-    printf("Input initial estimate of a root:\n");
-    scanf("%f", &x0);
-
-    while (1)
-    {
-        x = g(x0);
-        error = (x - x0) / x;
-
-        if (fabs(error) < E)
-        {
-            printf("\nRoot = %f", x0);
-            break;
-        }
-
-        x0 = x;
-    }
-
-    getch();
-    return 0;
+	 int step=1, N;
+	 float x0, x1, e;
+	 clrscr();
+	 /* Inputs */
+	 printf("Enter initial guess: ");
+	 scanf("%f", &x0);
+	 printf("Enter tolerable error: ");
+	 scanf("%f", &e);
+	 printf("Enter maximum iteration: ");
+	 scanf("%d", &N);
+	 /* Implementing Fixed Point Iteration */
+	 printf("\nStep\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
+	 do
+	 {
+		  x1 = g(x0);
+		  printf("%d\t%f\t%f\t%f\t%f\n",step, x0, f(x0), x1, f(x1));
+		
+		  step = step + 1;
+		
+		  if(step>N)
+		  {
+			   printf("Not Convergent.");
+			   exit(0);
+		  }
+		  
+		  x0 = x1;
+		  
+	 }while( fabs(f(x1)) > e);
+	 
+	 printf("\nRoot is %f", x1);
+	
+	 getch();
+	 return(0);
 }
