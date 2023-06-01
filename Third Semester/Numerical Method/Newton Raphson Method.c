4fd8d1a9102018fb75e059fb265474295b52e60f
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-01 12:13:20 +0545

    Update Newton Raphson Method.c

--- Third Semester/Numerical Method/Newton Raphson Method.c
+++ Third Semester/Numerical Method/Newton Raphson Method.c
@@ -1,38 +1,64 @@
-#include <stdio.h>
-#include <conio.h>
-#include <math.h>
 
-float f(float x)
-{
-    float y;
-    y = pow(x, 2) + x - 2;
-    return y;
-}
+/* Program: Finding real roots of nonlinear
+   equation using Newton Raphson Method
+   Author: CodeSansar
+   Date: November 18, 2018 */ 
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
 
-float fd(float x)
-{
-    float y;
-    y = 2 * x + 1;
-    return y;
-}
+/* Defining equation to be solved.
+   Change this equation to solve another problem. */
+#define    f(x)    3*x - cos(x) -1
+
+/* Defining derivative of g(x).
+   As you change f(x), change this function also. */
+#define   g(x)   3 + sin(x)
 
 void main()
 {
-    float x0, x1, error = 0.0001;
-    int i = 0;
+	 float x0, x1, f0, f1, g0, e;
+	 int step = 1, N;
+	 clrscr();
+     /* Inputs */
+	 printf("\nEnter initial guess:\n");
+	 scanf("%f", &x0);
+	 printf("Enter tolerable error:\n");
+	 scanf("%f", &e);
+	 printf("Enter maximum iteration:\n");
+	 scanf("%d", &N);
+	 /* Implementing Newton Raphson Method */
+	 printf("\nStep\t\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
+	 do
+	 {
+		  g0 = g(x0);
+		  f0 = f(x0);
+		  if(g0 == 0.0)
+		  {
+			   printf("Mathematical Error.");
+			   exit(0);
+		  }
 
-    printf("\nGuess initial root:");
-    scanf("%f", &x1);
+		
+		  x1 = x0 - f0/g0;
 
-    do
-    {
-        x0 = x1 - (f(x1) / fd(x1));
-        x1 = x0;
-        i++;
-    } while (fabs(f(x0)) > error);
-
-    printf("\nRoot = %f", x0);
-    printf("\nNumber of iterations = %d", i);
-
-    getch();
+		
+		  printf("%d\t\t%f\t%f\t%f\t%f\n",step,x0,f0,x1,f1);
+		  x0 = x1;
+		  
+		  step = step+1;
+		
+		  if(step > N)
+		  {
+			   printf("Not Convergent.");
+			   exit(0);
+		  }
+		  
+		  f1 = f(x1);
+		  
+	 }while(fabs(f1)>e);
+	
+	 printf("\nRoot is: %f", x1);
+	 getch();
 }
