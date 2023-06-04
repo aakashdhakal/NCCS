b26fa572c7e692f9f9ee05f77ff836cd74f81341
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-04 22:35:20 +0545

    Update infix to postfix conversion.c

--- Third Semester/Data Structure and Algorithm/infix to postfix conversion.c
+++ Third Semester/Data Structure and Algorithm/infix to postfix conversion.c
@@ -1,64 +1,104 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
 
-char stack[100];
-int top = -1;
+int isOperand(char);
+int isOperator(char);
+int priority(char);
+void conversion(char *);
 
-void push(char x)
+int isOperand(char ch)
 {
-    stack[++top] = x;
-}
-
-char pop()
-{
-    if(top == -1)
-        return -1;
-    else
-        return stack[top--];
-}
-
-int priority(char x)
-{
-    if(x == '(')
-        return 0;
-    if(x == '+' || x == '-')
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
         return 1;
-    if(x == '*' || x == '/')
-        return 2;
-    return 0;
+    else
+        return 0;
 }
 
-int main()
+int isOperator(char ch)
 {
-    char exp[100];
-    char *e, x;
-    printf("Enter the expression : ");
-    scanf("%s",exp);
-    printf("\n");
-    e = exp;
-    printf("The postfix expression is: ");
-    while(*e != '\0')
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '$')
+        return 1;
+    else
+        return 0;
+}
+
+int priority(char ch)
+{
+    if (ch == '$')
+        return 3;
+    if (ch == '*' || ch == '/')
+        return 2;
+    if (ch == '+' || ch == '-')
+        return 1;
+    else
+        return 0;
+}
+
+void conversion(char *ch)
+{
+    char opstack[100], poststring[100];
+    int i = 0, top = -1;
+    while (*ch != '\0')
     {
-        if(isalnum(*e))
-            printf("%c ",*e);
-        else if(*e == '(')
-            push(*e);
-        else if(*e == ')')
+        if (isOperand(*ch))
         {
-            while((x = pop()) != '(')
-                printf("%c ", x);
+            poststring[i++] = *ch;
         }
-        else
+        else if (isOperator(*ch))
         {
-            while(priority(stack[top]) >= priority(*e))
-                printf("%c ",pop());
-            push(*e);
+            if (top == -1)
+            {
+                opstack[++top] = *ch;
+            }
+            else if (*ch == '(')
+            {
+                opstack[++top] = *ch;
+            }
+            else if (priority(*ch) > priority(opstack[top]))
+            {
+                opstack[++top] = *ch;
+            }
+            else
+            {
+                while (priority(*ch) <= priority(opstack[top]) && top != -1 && opstack[top] != '(')
+                {
+                    poststring[i++] = opstack[top--];
+                }
+                opstack[++top] = *ch;
+            }
         }
-        e++;
+        else if (*ch == '(')
+        {
+            opstack[++top] = *ch;
+        }
+        else if (*ch == ')')
+        {
+            while (opstack[top] != '(' && top != -1)
+            {
+                poststring[i++] = opstack[top--];
+            }
+            if (top != -1 && opstack[top] == '(')
+            {
+                top--;
+            }
+        }
+        ch++;
     }
-    
-    while(top != -1)
+
+    while (top != -1)
     {
-        printf("%c ",pop());
-    }return 0;
+        poststring[i++] = opstack[top--];
+    }
+    poststring[i] = '\0';
+
+    printf("The postfix expression is: %s\n", poststring);
+}
+
+void main()
+{
+    char infix[100];
+    printf("Enter the infix expression: ");
+    gets(infix);
+    conversion(infix);
 }
