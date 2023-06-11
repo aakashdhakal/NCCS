7ed063cb472163a2a5386f549a518ca045028aaf
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-11 20:34:04 +0545

    Update Horner's Method.c

--- Third Semester/Numerical Method/Horner's Method.c
+++ Third Semester/Numerical Method/Horner's Method.c
@@ -2,29 +2,28 @@
 
 int main()
 {
-     float a[100],sum=0,x;
-     int n,i;
+   float a[100], sum = 0, x;
+   int n, i;
 
-     printf("\nEnter degree of the polynomial X :: ");
-     scanf("%d",&n);
-     printf("\nEnter coefficient's of the polynomial X :: \n");
-     for(i=n;i>=0;i--)
-     {
-            printf("\nEnter Coefficient of [ X^%d ] :: ",i);
-            scanf("%f",&a[i]);
-     }
+   printf("\nEnter degree of the polynomial X : ");
+   scanf("%d", &n);
+   for (i = n; i >= 0; i--)
+   {
+      printf("\nEnter Coefficient of [ X^%d ] : ", i);
+      scanf("%f", &a[i]);
+   }
 
-     printf("\nEnter the value of X :: ");
-     scanf("%f",&x);
+   printf("\nEnter the value of X : ");
+   scanf("%f", &x);
 
-     for(i=n;i>0;i--)
-     {
-        sum=(sum+a[i])*x;
-     }
+   for (i = n; i > 0; i--)
+   {
+      sum = (sum + a[i]) * x;
+   }
 
-     sum=sum+a[0];
+   sum = sum + a[0];
 
-     printf("\nValue of the polynomial is = [ %f ]\n",sum);
+   printf("\nValue of the polynomial is = %f\n", sum);
 
-     return 0;
+   return 0;
 }
