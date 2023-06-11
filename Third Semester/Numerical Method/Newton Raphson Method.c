6dc144224baa608dfc17bec90949cabb4220f6cf
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-11 20:23:26 +0545

    Update Newton Raphson Method.c

--- Third Semester/Numerical Method/Newton Raphson Method.c
+++ Third Semester/Numerical Method/Newton Raphson Method.c
@@ -1,64 +1,62 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
 
-/* Program: Finding real roots of nonlinear
-   equation using Newton Raphson Method
-   Author: CodeSansar
-   Date: November 18, 2018 */ 
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-#include<stdlib.h>
-
-/* Defining equation to be solved.
-   Change this equation to solve another problem. */
-#define    f(x)    3*x - cos(x) -1
-
-/* Defining derivative of g(x).
-   As you change f(x), change this function also. */
-#define   g(x)   3 + sin(x)
-
-void main()
+float f(float x)
 {
-	 float x0, x1, f0, f1, g0, e;
-	 int step = 1, N;
-	 clrscr();
-     /* Inputs */
-	 printf("\nEnter initial guess:\n");
-	 scanf("%f", &x0);
-	 printf("Enter tolerable error:\n");
-	 scanf("%f", &e);
-	 printf("Enter maximum iteration:\n");
-	 scanf("%d", &N);
-	 /* Implementing Newton Raphson Method */
-	 printf("\nStep\t\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
-	 do
-	 {
-		  g0 = g(x0);
-		  f0 = f(x0);
-		  if(g0 == 0.0)
-		  {
-			   printf("Mathematical Error.");
-			   exit(0);
-		  }
+	return (pow(x, 2) + 2 * x - 2);
+}
 
-		
-		  x1 = x0 - f0/g0;
+float g(float x)
+{
+	return (2 * x + 2);
+}
 
-		
-		  printf("%d\t\t%f\t%f\t%f\t%f\n",step,x0,f0,x1,f1);
-		  x0 = x1;
-		  
-		  step = step+1;
-		
-		  if(step > N)
-		  {
-			   printf("Not Convergent.");
-			   exit(0);
-		  }
-		  
-		  f1 = f(x1);
-		  
-	 }while(fabs(f1)>e);
-	
-	 printf("\nRoot is: %f", x1);
-	 getch();
+int main()
+{
+	float x0, x1, f0, f1, g0, e;
+	int step = 1, N;
+
+	printf("\nEnter initial guess: ");
+	scanf("%f", &x0);
+
+	printf("Enter tolerable error: ");
+	scanf("%f", &e);
+
+	printf("Enter maximum iteration: ");
+	scanf("%d", &N);
+
+	printf("\n\tStep\t\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
+
+	do
+	{
+		g0 = g(x0);
+		f0 = f(x0);
+
+		if (g0 == 0.0)
+		{
+			printf("Mathematical Error.");
+			exit(0);
+		}
+
+		x1 = x0 - f0 / g0;
+		f1 = f(x1);
+
+		printf("\t%d\t\t%f\t%f\t%f\t%f\n", step, x0, f0, x1, f1);
+
+		x0 = x1;
+		step = step + 1;
+
+		if (step > N)
+		{
+			printf("Not Convergent.");
+			exit(0);
+		}
+
+	} while (fabs(f1) > e);
+
+	printf("\nRoot is: %f", x1);
+	getch();
+	return 0;
 }
