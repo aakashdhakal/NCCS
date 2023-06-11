554bc3ef06bc75d704a4efc47577682c10aa0d78
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-11 19:38:32 +0545

    Update Bisection Method.c

--- Third Semester/Numerical Method/Bisection Method.c
+++ Third Semester/Numerical Method/Bisection Method.c
@@ -1,57 +1,50 @@
-/* Program: Finding real roots of nonlinear
-   equation using Bisection Method
-   Author: CodeSansar
-   Date: November 18, 2018 */
-/* Header Files */
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-/*
- Defining equation to be solved.
- Change this equation to solve another problem.
-*/
-#define f(x) cos(x) - x * exp(x)
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+float f(float x)
+{
+	return (pow(x, 3) - 4 * x - 9);
+}
 
 void main()
 {
-	 float x0, x1, x2, f0, f1, f2, e;
-	 int step = 1;
-	 /* Inputs */
-	 up:
-	 printf("\nEnter two initial guesses:\n");
-	 scanf("%f%f", &x0, &x1);
-	 printf("Enter tolerable error:\n");
-	 scanf("%f", &e);
-	 /* Calculating Functional Value */
-	 f0 = f(x0);
-	 f1 = f(x1);
-	 /* Checking whether given guesses brackets the root or not. */
-	 if( f0 * f1 > 0.0)
-	 {
-		  printf("Incorrect Initial Guesses.\n");
-		  goto up;
-	 }
-   /* Implementing Bisection Method */
-	 printf("\nStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
-	 do
-	 {
-		  x2 = (x0 + x1)/2;
-		  f2 = f(x2);
-		
-		  printf("%d\t\t%f\t%f\t%f\t%f\n",step, x0, x1, x2, f2);
-		
-		  if( f0 * f2 < 0)
-		  {
-			   x1 = x2;
-			   f1 = f2;
-		  }
-		  else
-		  {
-			   x0 = x2;
-			   f0 = f2;
-		  }
-		  step = step + 1;
-	 }while(fabs(f2)>e);
-	 printf("\nRoot is: %f", x2);
-	 getch();
+	float x0, x1, x2, f0, f1, f2, e;
+	int step = 1;
+	do
+	{
+		printf("\nEnter two initial guesses [a,b]: ");
+		scanf("%f%f", &x0, &x1);
+		if (f0 * f1 > 0.0)
+		{
+			printf("Incorrect Initial Guesses.\n");
+		}
+	} while (f0 * f1 > 0.0);
+	printf("Enter tolerable error: ");
+	scanf("%f", &e);
+	f0 = f(x0);
+	f1 = f(x1);
+
+	printf("\n\tStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
+	do
+	{
+		x2 = (x0 + x1) / 2;
+		f2 = f(x2);
+
+		printf("\t%d\t\t%f\t%f\t%f\t%f\n", step, x0, x1, x2, f2);
+
+		if (f0 * f2 < 0)
+		{
+			x1 = x2;
+			f1 = f2;
+		}
+		else
+		{
+			x0 = x2;
+			f0 = f2;
+		}
+		step = step + 1;
+	} while (fabs(f2) > e);
+	printf("\nRoot is: %f", x2);
+	getch();
 }
