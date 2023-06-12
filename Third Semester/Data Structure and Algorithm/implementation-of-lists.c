9acbbcb64334b27fabe5e5f6675867e6cebb640b
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-12 22:58:53 +0545

    Add files via upload

---
+++ Third Semester/Data Structure and Algorithm/implementation-of-lists.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#define MAX_SIZE 5
+
+struct list
+{
+    int data[MAX_SIZE];
+    int size;
+};
+
+typedef struct list listType;
+
+void insert(listType *);
+void printList(listType *);
+void deleteElement(listType *);
+void search(listType *);
+void modify(listType *);
+
+int main()
+{
+    listType list;
+    list.size = 0;
+    int choice;
+    do
+    {
+        printf("\n1. Insert\n2. Delete\n3. Search\n4. Modify\n5. Print\n6. Exit\n\nEnter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            insert(&list);
+            break;
+        case 2:
+            deleteElement(&list);
+            break;
+        case 3:
+            search(&list);
+            break;
+        case 4:
+            modify(&list);
+            break;
+        case 5:
+            printList(&list);
+            break;
+        case 6:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("Invalid choice\n");
+        }
+    } while (choice != 6);
+}
+
+void insert(listType *list)
+{
+    if (list->size == MAX_SIZE - 1)
+    {
+        printf("List is full\n");
+        return;
+    }
+    int data, pos;
+    printf("Enter data: ");
+    scanf("%d", &data);
+    printf("Enter position [1 - %d]: ", MAX_SIZE);
+    scanf("%d", &pos);
+    if (pos > MAX_SIZE || pos < 1)
+    {
+        printf("Invalid position\n");
+        return;
+    }
+    else
+    {
+        for (int i = list->size - 1; i >= pos; i--)
+        {
+            list->data[i + 1] = list->data[i];
+        }
+        list->data[pos - 1] = data;
+        list->size++;
+    }
+    printf("%d is inserted in the position %d of the list\n", data, pos);
+}
+void deleteElement(listType *list)
+{
+    if (list->size == 0)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    int pos, value;
+    printf("Enter position [1 - %d]: ", list->size);
+    scanf("%d", &pos);
+    if (pos > list->size || pos < 1)
+    {
+        printf("Invalid position\n");
+        return;
+    }
+    else
+    {
+        value = list->data[pos];
+        for (int i = pos; i < list->size; i++)
+        {
+            list->data[i] = list->data[i + 1];
+        }
+        list->size--;
+    }
+    printf("The value %d at position %d is deleted\n", pos);
+}
+void modify(listType *list)
+{
+    int value, pos;
+    printf("Enter position [0 - %d]: ", list->size);
+    scanf("%d", &pos);
+    printf("Enter new value: ");
+    scanf("%d", &value);
+    if (pos > list->size || pos < 0)
+    {
+        printf("Invalid position\n");
+        return;
+    }
+    else
+    {
+        list->data[pos] = value;
+    }
+    printf("The value at position %d is modified to %d\n", pos, value);
+}
+void search(listType *list)
+{
+    int value;
+    printf("Enter value to search: ");
+    scanf("%d", &value);
+    for (int i = 0; i < list->size; i++)
+    {
+        if (list->data[i] == value)
+        {
+            printf("The value found at position %d\n", i);
+            return;
+        }
+    }
+    printf("The value is not found\n");
+}
+
+void printList(listType *list)
+{
+    if (list->size == 0)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    printf("List: ");
+    for (int i = 0; i < list->size; i++)
+    {
+        printf("%d:%d ", list->data[i], i);
+    }
+    printf("\n");
+}
