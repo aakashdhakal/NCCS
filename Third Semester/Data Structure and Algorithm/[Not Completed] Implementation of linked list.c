a5558fc14e0ce3150e34bbe1e28ea40c0bf0545d
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-21 16:12:53 +0545

    Update [Not Completed] Implementation of linked list.c

--- Third Semester/Data Structure and Algorithm/[Not Completed] Implementation of linked list.c
+++ Third Semester/Data Structure and Algorithm/[Not Completed] Implementation of linked list.c
@@ -9,26 +9,43 @@ struct node
 
 typedef struct node Node;
 Node *start = NULL;
-void insert(Node *);
+void insert();
+void display();
 
 int main()
 {
 
-    insert(&start);
+     int option;
+   
+    do{
+    	 printf("\n 1. Insert \n 2. Display\n 3. Exit\n");
+    printf("\nEnter your option: ");
+    scanf("%d", &option);
+    switch (option)
+    {
+    case 1:
+       insert();
+        break;
+    case 2:
+    	display();
+        break;
+    case 3:
+        break;
+    default:
+        printf("Invalid option\n");
+        break;
+    }
+}while(option != 3);
     return 0;
 }
 
-void insert(Node *newnode)
+void insert()
 {
-    newnode = (Node *)malloc(sizeof(Node));
+    Node *newnode = (Node *)malloc(sizeof(Node));
+    Node *temp;
+    int pos,i;
     printf("Enter the data: ");
     scanf("%d", &newnode->data);
-    newnode->next = NULL;
-    if (start == NULL)
-    {
-        newnode->next = NULL;
-        start = newnode;
-    }
     int option;
     printf("\n 1. Insert at the beginning\n 2. Insert at the end\n 3. Insert at a given position\n");
     printf("Enter your option: ");
@@ -40,26 +57,37 @@ void insert(Node *newnode)
         start = newnode;
         break;
     case 2:
-        Node *temp = start;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        temp->next = newnode;
+        temp = start;
+        while(temp->next != NULL){
+        	temp = temp->next;
+		}
+		temp->next = newnode;
+		newnode->next = NULL;
         break;
     case 3:
-        int pos;
+        
         printf("Enter the position: ");
         scanf("%d", &pos);
-        Node *temp = start;
-        for (int i = 1; i < pos - 1; i++)
-        {
-            temp = temp->next;
-        }
-        newnode->next = temp->next;
-        temp->next = newnode;
+        i = 0;
+        while(i=pos-1){
+        	temp = temp->next;
+        	i++;
+		}
+		newnode->next = temp->next;
+		temp->next = newnode;
+		
         break;
     default:
         printf("Invalid option\n");
         break;
     }
+}
+
+void display(){
+	Node *temp;
+	temp = start;
+	while(temp->next == NULL){
+		printf("%d\n", temp->data);
+		temp = temp->next;
+	}
+}
