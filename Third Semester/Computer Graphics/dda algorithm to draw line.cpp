820db35d8f9968c173570db9a14b1618aad1f900
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-21 21:58:13 +0545

    Update dda algorithm to draw line.cpp

--- Third Semester/Computer Graphics/dda algorithm to draw line.cpp
+++ Third Semester/Computer Graphics/dda algorithm to draw line.cpp
@@ -1,48 +1,59 @@
-#include<GL/glut.h>
-#include <GL/gl.h>
-#include<stdlib.h>
-#include<stdio.h>
-#define ROUND(x) ((int)(x+0.5))
-int xa,xb,ya,yb;
-void init(void){
-glClearColor (0.0, 0.0, 0.0, 1.0);
-glOrtho(-100.0, 100.0, -100.0, 100.0, -1.0, 1.0);
+#include <GL/glut.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void plotPixel()
+{
+    float xi, yi, xf, yf, x, y, xinc, yinc,dx,dy;
+    int steps,i;
+    cout << "Enter the initial points: ";
+    cin >> xi >> yi;
+    cout << "Enter the final points: ";
+    cin >> xf >> yf;
+    x = xi;
+    y = yi;
+    dx = xf - xi;
+    dy = yf - yi;
+    if(abs(dx) > abs(dy)){
+    	steps = abs(dx);
+	}
+	else{
+		steps = abs(dy);
+	}
+    xinc = dx/(float)steps;
+    yinc = dy/(float)steps;
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_POINTS);
+	i = 0;
+    while (i <= steps) {
+        glVertex2f(round(x), round(y));
+        x += xinc;
+        y += yinc;
+        i++;
+    }
+
+    glEnd();
+    glFlush();
 }
-void display (void){
-//DDA Algorithm starts here
-int dx = xb-xa,dy = yb-ya,steps,k;
-float xIncrement,yIncrement, x = xa, y = ya;
-if(abs(dx)>abs(dy))
-steps = abs(dx);
-else steps = abs(dy);
-xIncrement = dx/(float)steps;
-yIncrement = dy/(float)steps;
 
-glClear (GL_COLOR_BUFFER_BIT);
-glColor3f (1.0, 1.0, 1.0);
-glBegin(GL_POINTS);
-glVertex2s(ROUND(x),ROUND(y));
-for(k=0;k<steps;k++){
-x = x+xIncrement;
-y = y+yIncrement;
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
 
-glVertex2s(ROUND(x),ROUND(y));
-}
-glEnd();
-glutSwapBuffers();
-}
-
-int main(int argc, char** argv){
-
-printf("Enter the points\n");
-scanf("%d %d %d %d",&xa,&ya,&xb,&yb);
-glutInit(&argc, argv);
-glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-glutInitWindowSize (500, 500);
-glutInitWindowPosition (100, 100);
-glutCreateWindow ("Simple DDA ");
-init ();
-glutDisplayFunc(display);
-glutMainLoop();
-return 0;
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Drawing Shapes");
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 800, 0, 600);
+    glutDisplayFunc(plotPixel);
+    glutMainLoop();
+    
+    return 0;
 }
