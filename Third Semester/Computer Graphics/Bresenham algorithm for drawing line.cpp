756cb599967041332f778aa00f85b50667ceaf47
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-22 12:30:10 +0545

    Create Bresenham algorithm for drawing line.cpp

---
+++ Third Semester/Computer Graphics/Bresenham algorithm for drawing line.cpp
@@ -0,0 +1,65 @@
+#include <GL/glut.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+ float xi, yi, xf, yf, x, y, xinc, yinc,dx,dy,p,xend;
+void plotPixel()
+{
+   
+    int steps,i;
+    cout << "Enter the initial points: ";
+    cin >> xi >> yi;
+    cout << "Enter the final points: ";
+    cin >> xf >> yf;
+    dx = xf - xi;
+    dy = yf - yi;   
+    p = 2*dy - dx;
+    
+    if(xi > xf){
+    	x = xf;
+    	y = yf;
+    	xend = xi;
+	}
+	else{
+		x = xi;
+		y = yi;
+		xend = xf;
+	}
+	while(x <= xend){
+		if(p < 0 ){
+			p += 2*dy;
+			x += 1;
+			y = y; 
+		}
+		else{
+			p += 2*dy - 2*dx;
+			x += 1;
+			y -= 1;   
+		}
+	}
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_POINTS);
+
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Drawing Shapes");
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 800, 0, 600);
+    glutDisplayFunc(plotPixel);
+    glutMainLoop();
+    
+    return 0;
+}
