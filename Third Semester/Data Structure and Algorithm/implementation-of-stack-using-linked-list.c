570c2262d974178d4fd2dc58ed7b3584b2970f7d
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-24 18:09:14 +0545

    Update implementation-of-stack-using-linked-list.c

--- Third Semester/Data Structure and Algorithm/implementation-of-stack-using-linked-list.c
+++ Third Semester/Data Structure and Algorithm/implementation-of-stack-using-linked-list.c
@@ -1,91 +1,84 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct stack{
-	int data;
-	struct stack *next;
+struct node
+{
+    int data;
+    struct node *next;
 };
-typedef struct stack node;
+typedef struct node Node;
+Node *top = NULL;
 
-void push(node*);
-void pop(node*);
-void display(node*);
+void push();
+void pop();
+void display();
 
-int main(){	
-	int option;
-	node *top = NULL;
-	
-	printf("Menu: \n1.Push \n2.Pop \n3.Display \n4.Exit \n\n");
-	do{
-		printf("\nEnter an option: ");
-		scanf("%d",&option);
-		
-		switch (option){
-			case 1:
-				push(top);
-				break;
-			
-			case 2:
-				pop(top);
-				break;
-			
-			case 3:
-				display(top);
-				break;
-				
-			case 4:
-				printf("Exiting...");
-				break;
-				
-			default:
-				printf("Invalid input\n");
-				break;
-		}
+int main()
+{
+    int option;
+    printf("\n 1. Push\n 2. Pop\n 3. Display\n 4. Exit\n");
 
-	}while(option != 4);
-
-	return 0;
+    do
+    {
+        printf("\nEnter your option: ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            break;
+        default:
+            printf("Invalid option\n");
+            break;
+        }
+    } while (option != 4);
+    return 0;
 }
 
-void push(node *stop){
-	if(stop != NULL){
-		printf("Not enough memory\n");
-		return;
-	}
-	int value;
-	printf("Enter the value to be pushed: ");
-	scanf("%d",&value);
-	node *newnode = (node*)malloc(sizeof(node));
-	newnode->data = value;
-	newnode->next = stop;
-	stop = newnode;
+void push()
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    printf("Enter the data: ");
+    scanf("%d", &newNode->data);
+    newNode->next = top;
+    top = newNode;
+    printf("%d is pushed into stack\n", newNode->data);
 }
 
-void pop(node *stop){
-	if(stop == NULL){
-		printf("Stack Underflow");
-		return;
-	}
-	printf("The popped value is %d",stop->data);
-	
-	node* temp;
-	temp = stop;
-	stop = stop->next;
-	free(temp);
+void pop()
+{
+    if (top == NULL)
+    {
+        printf("Stack underflow\n");
+        return;
+    }
+    Node *temp = top;
+    top = top->next;
+    printf("%d is popped from stack\n", temp->data);
+    free(temp);
 }
 
-void display(node *stop){
-	if(stop == NULL){
-		printf("Stack Underflow");
-		return;
-	}
-	
-	node *temp;
-	temp = stop;
-	printf("The data of the stack are: ");
-	while(temp != NULL){
-		printf("%d\t",temp->next);
-		temp = temp->next;
-		
-	}
+void display()
+{
+    if (top == NULL)
+    {
+        printf("Stack underflow\n");
+        return;
+    }
+    Node *temp = top;
+    printf("Stack: ");
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
 }
