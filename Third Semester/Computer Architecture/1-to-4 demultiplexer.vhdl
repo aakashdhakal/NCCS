f8e9b99d4d299bd8063e48505c6e3caa6638e522
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-26 14:06:29 +0545

    Update 1-to-4 demultiplexer.vhdl

--- Third Semester/Computer Architecture/1-to-4 demultiplexer.vhdl
+++ Third Semester/Computer Architecture/1-to-4 demultiplexer.vhdl
@@ -2,23 +2,22 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-entity DEMUX is
-Port ( I : in STD_LOGIC;
-S : in STD_LOGIC_VECTOR (1 downto 0);
-Y : out STD_LOGIC_VECTOR (3 downto 0));
-end DEMUX;
-architecture Behavioral of DEMUX is
+entity demultiplexer is
+Port ( i : in STD_LOGIC;
+s0 : in STD_LOGIC;
+s1 : in STD_LOGIC;
+a : out STD_LOGIC;
+b : out STD_LOGIC;
+c : out STD_LOGIC;
+d : out STD_LOGIC);
+end demultiplexer;
+architecture Behavioral of demultiplexer is
+signal p,q : STD_LOGIC;
 begin
-process (I, S)
-begin
-if (S <= "00") then
-Y(0) <= I ;
-elsif (S <= "01") then
-Y(1) <= I ;
-elsif (S <= "10") then
-Y(2) <= I ;
-else
-Y(3) <= I ;
-end if;
-end process;
+p <= not s0;
+q <= not s1;
+a<= i and p and q;
+b<= i and q and s0;
+c<= i and p and s1;
+d<= i and s1 and s0;
 end Behavioral;
