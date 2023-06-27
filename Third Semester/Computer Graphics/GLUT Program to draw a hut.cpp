a88b801b52f92122a68a1f3dd30fb72845d6197a
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-27 22:04:47 +0545

    Update GLUT Program to draw a hut.cpp

--- Third Semester/Computer Graphics/GLUT Program to draw a hut.cpp
+++ Third Semester/Computer Graphics/GLUT Program to draw a hut.cpp
@@ -1,139 +1,77 @@
+#include <GL/glut.h>
 
-#include <GL\glut.h>
-using namespace std;
-
-void myInit(void)
+void drawHut()
 {
-	glClearColor(1.0, 0.5, 0.0, 1.0);
+    // Draw the roof of the hut
+    glColor3f(0.6f, 0.8f, 1.0f); // Light blue color
+    glBegin(GL_TRIANGLES);
 
-	glMatrixMode(GL_PROJECTION);
+    glVertex2f(-170.0f, 50.0f); // Bottom-left vertex
+    glVertex2f(170.0f, 50.0f);  // Bottom-right vertex
+    glVertex2f(0.0f, 200.0f);   // Top vertex
+    glEnd();
 
-	glLoadIdentity();
+    // Draw the walls of the hut
+    glColor3f(0.0f, 1.3f, 0.0f); // Red color
+    glBegin(GL_POLYGON);
+    glVertex2f(-170.0f, -250.0f); // Bottom-left vertex
+    glVertex2f(-170.0f, 50.0f);   // Top-left vertex
+    glVertex2f(170.0f, 50.0f);    // Top-right vertex
+    glVertex2f(170.0f, -250.0f);  // Bottom-right vertex
+    glEnd();
 
-	gluOrtho2D(0.0, 400.0, 0.0, 400.0);
+    // Draw the door of the hut
+    glColor3f(0.6f, 0.8f, 1.0f); // Light blue color
+    glBegin(GL_POLYGON);
+    glVertex2f(-60.0f, -240.0f); // Bottom-left vertex
+    glVertex2f(-60.0f, -100.0f); // Top-left vertex
+    glVertex2f(60.0f, -100.0f);  // Top-right vertex
+    glVertex2f(60.0f, -240.0f);  // Bottom-right vertex
+    glEnd();
+
+    // Draw the window of the hut
+    glColor3f(1.0f, 0.0f, 0.0f); // Red color
+    glBegin(GL_POLYGON);
+    glVertex2f(40.0f, -80.0f);  // Bottom-right vertex
+    glVertex2f(40.0f, -10.0f);  // Top-right vertex
+    glVertex2f(120.0f, -10.0f); // Top-left vertex
+    glVertex2f(120.0f, -80.0f); // Bottom-left vertex
+    glEnd();
+
+    // Draw the window of the hut
+    glBegin(GL_POLYGON);
+    glVertex2f(-40.0f, -80.0f);  // Bottom-right vertex
+    glVertex2f(-40.0f, -10.0f);  // Top-right vertex
+    glVertex2f(-120.0f, -10.0f); // Top-left vertex
+    glVertex2f(-120.0f, -80.0f); // Bottom-left vertex
+    glEnd();
 }
 
-void myDisplay(void)
+void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
 
-	glPointSize(4.0);
+    // Translate the hut to the center of the window
+    glTranslatef(400.0f, 300.0f, -1.0f);
 
-	glColor3f(0.5f, 0.5f, 0.5f);
+    drawHut();
 
-	glBegin(GL_POLYGON);
-
-	glVertex2i(40, 40);
-	glVertex2i(320, 40);
-
-	glVertex2i(40, 200);
-	glVertex2i(320, 200);
-
-	glVertex2i(40, 200);
-	glVertex2i(40, 40);
-
-	glVertex2i(320, 200);
-	glVertex2i(320, 40);
-
-	glEnd();
-
-	glColor3f(1.0f, 0.0f, 0.0f);
-
-	glBegin(GL_POLYGON);
-
-	glVertex2i(220, 60);
-	glVertex2i(300, 60);
-
-	glVertex2i(220, 150);
-	glVertex2i(300, 150);
-
-	glVertex2i(220, 60);
-	glVertex2i(220, 150);
-
-	glVertex2i(300, 150);
-	glVertex2i(300, 60);
-
-	glEnd();
-
-	glColor3f(1.0f, 0.0f, 0.0f);
-
-	glBegin(GL_POLYGON);
-
-	glVertex2i(220, 170);
-	glVertex2i(300, 170);
-
-	glVertex2i(220, 190);
-	glVertex2i(300, 190);
-
-	glVertex2i(220, 170);
-	glVertex2i(220, 190);
-
-	glVertex2i(300, 190);
-	glVertex2i(300, 170);
-
-	glEnd();
-
-	glColor3f(0.60f, 0.42f, 0.16f);
-
-	glBegin(GL_POLYGON);
-
-	glVertex2i(130, 40);
-	glVertex2i(130, 160);
-
-	glVertex2i(130, 160);
-	glVertex2i(180, 160);
-
-	glVertex2i(180, 100);
-	glVertex2i(180, 40);
-
-	glVertex2i(120, 40);
-	glVertex2i(170, 40);
-
-	glEnd();
-
-	glColor3f(0.60f, 0.42f, 0.16f);
-
-	glBegin(GL_POLYGON);
-
-	glVertex2i(130, 170);
-	glVertex2i(130, 180);
-
-	glVertex2i(130, 180);
-	glVertex2i(180, 180);
-
-	glVertex2i(180, 170);
-	glVertex2i(180, 180);
-
-	glVertex2i(130, 170);
-	glVertex2i(180, 170);
-
-	glEnd();
-
-	glColor3f(1.0f, 0.0f, 1.0f);
-
-	glBegin(GL_POLYGON);
-	glVertex2i(10, 200);
-	glVertex2i(340, 200);
-
-	glVertex2i(200, 390);
-
-	glVertex2i(10, 200);
-	glVertex2i(200, 390);
-
-	glEnd();
-	glFlush();
+    glFlush();
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(
-	GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(1200, 740);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Aakash Dhakal");
-	glutDisplayFunc(myDisplay);
-	myInit();
-	glutMainLoop();
-	return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Hut");
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 800, 0, 600);
+    glMatrixMode(GL_MODELVIEW);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
