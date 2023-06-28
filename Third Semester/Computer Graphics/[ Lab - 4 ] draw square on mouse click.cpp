d572cad21a73b1468b5af1631cf5c587f8ba809b
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-06-28 21:40:08 +0545

    Update [ Lab - 4 ] draw square on mouse click.cpp

--- Third Semester/Computer Graphics/[ Lab - 4 ] draw square on mouse click.cpp
+++ Third Semester/Computer Graphics/[ Lab - 4 ] draw square on mouse click.cpp
@@ -1,31 +1,22 @@
-//not cannon according to lab
 #include <GL/glut.h>
 
 int windowWidth = 800;
 int windowHeight = 600;
-int squareSize = 50;
+int squareSize = 70;
 int mouseX = 0;
 int mouseY = 0;
-bool mouseClicked = false;
-
-void drawSquare(int x, int y)
-{
-    glColor3f(1.0f, 0.0f, 0.0f); // Red square
-    glBegin(GL_POLYGON);
-    glVertex2f(x - squareSize / 2, y - squareSize / 2);
-    glVertex2f(x + squareSize / 2, y - squareSize / 2);
-    glVertex2f(x + squareSize / 2, y + squareSize / 2);
-    glVertex2f(x - squareSize / 2, y + squareSize / 2);
-    glEnd();
-}
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    if (mouseClicked)
-    {
-        drawSquare(mouseX, windowHeight - mouseY); // Flip Y coordinate to match OpenGL's coordinate system
-    }
+
+    glColor3f(1.0f, 0.0f, 0.0f); // Red square
+    glBegin(GL_POLYGON);
+    glVertex2f(mouseX, windowHeight - mouseY);
+    glVertex2f(mouseX + squareSize, windowHeight - mouseY);
+    glVertex2f(mouseX + squareSize, windowHeight - mouseY + squareSize);
+    glVertex2f(mouseX, windowHeight - mouseY + squareSize);
+    glEnd();
     glutSwapBuffers();
 }
 
@@ -33,9 +24,9 @@ void mouse(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
-        mouseX = x;
-        mouseY = y;
-        mouseClicked = true;
+        mouseX = x - 2;
+        mouseY = y - 2;
+        display();
         glutPostRedisplay();
     }
 }
