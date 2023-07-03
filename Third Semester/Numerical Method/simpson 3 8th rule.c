2a65d339d602dfb1d95757dc15b3bac3bc70c616
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-07-03 11:40:59 +0545

    Create simpson 3 8th rule.c

---
+++ Third Semester/Numerical Method/simpson 3 8th rule.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+// यो भित्र को function कृपया change गर्दिनु होला !! धन्यबाद !!
+double f(double x){
+	return pow(x,2)+ 2*x;
+}
+
+int main()
+{
+ float lower, upper, integration=0.0, stepSize, k;
+ int i, subInterval;
+ /* Input */
+ printf("Enter lower limit of integration: ");
+ scanf("%f", &lower);
+ printf("Enter upper limit of integration: ");
+ scanf("%f", &upper);
+ do{
+ 	printf("Enter number of sub intervals: ");
+ 	 scanf("%d", &subInterval);
+ 	if(subInterval%3 != 0){
+ 		printf("\nThe subintervals should bemultiple of 3\n");
+ }
+}while(subInterval%3 != 0);
+
+ /* Calculation */
+ /* Finding step size */
+ stepSize = (upper - lower)/subInterval;
+
+ /* Finding Integration Value */
+ integration = f(lower) + f(upper);
+ for(i=1; i<= subInterval-1; i++)
+ {
+  k = lower + i*stepSize;
+  if(i%3 == 0)
+  {
+   integration = integration + 2 * f(k);
+  }
+  else
+  {
+   integration = integration + 3 * f(k);
+  }
+ }
+ integration = integration * stepSize*3/8;
+ printf("\nRequired value of integration is: %.3f", integration);
+ getch();
+ return 0;
+}
