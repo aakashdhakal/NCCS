f900c1cbbe47886a09ab95b25205e5a504336ed0
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-07-03 11:16:50 +0545

    Create trapezoidal rule.c

---
+++ Third Semester/Numerical Method/trapezoidal rule.c
@@ -0,0 +1,39 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+/* Define function here */
+double f(double x){
+	return pow(x,2)+ 2*x;
+}
+
+int main()
+{
+ float lower, upper, integration=0.0, stepSize, k;
+ int i, subInterval;
+
+ /* Input */
+ printf("Enter lower limit of integration: ");
+ scanf("%f", &lower);
+ printf("Enter upper limit of integration: ");
+ scanf("%f", &upper);
+ printf("Enter number of sub intervals: ");
+ scanf("%d", &subInterval);
+
+ /* Calculation */
+ /* Finding step size */
+ stepSize = (upper - lower)/subInterval;
+
+ /* Finding Integration Value */
+ integration = f(lower) + f(upper);
+ for(i=1; i<= subInterval-1; i++)
+ {
+  k = lower + i*stepSize;
+  integration = integration + 2 * f(k);
+ }
+ integration = integration * stepSize/2;
+ printf("\nRequired value of integration is: %.3f", integration);
+ getch();
+ return 0;
+}
