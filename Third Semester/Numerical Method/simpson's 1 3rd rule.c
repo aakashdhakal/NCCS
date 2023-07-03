76440259c4f06c4106f12758f5b4f26d3d138732
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-07-03 11:24:22 +0545

    Create simpson's 1 3rd rule.c

---
+++ Third Semester/Numerical Method/simpson's 1 3rd rule.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+/* Define function here */
+double f(double x){
+	return pow(x,2)+ 2*x;
+}
+
+int main()
+{
+ float lower, upper, integration=0.0, stepSize, k;
+ int i, subInterval;
+ /* Input */
+ printf("Enter lower limit of integration: ");
+ scanf("%f", &lower);
+ printf("Enter upper limit of integration: ");
+ scanf("%f", &upper);
+ printf("Enter number of sub intervals: ");
+ scanf("%d", &subInterval);
+
+ /* Calculation */
+ /* Finding step size */
+ stepSize = (upper - lower)/subInterval;
+
+ /* Finding Integration Value */
+ integration = f(lower) + f(upper);
+ for(i=1; i<= subInterval-1; i++)
+ {
+  k = lower + i*stepSize;
+  if(i%2==0)
+  {
+   integration = integration + 2 * f(k);
+  }
+  else
+  {
+   integration = integration + 4 * f(k);
+  }
+ }
+ integration = integration * stepSize/3;
+ printf("\nRequired value of integration is: %.3f", integration);
+ getch();
+ return 0;
+}
