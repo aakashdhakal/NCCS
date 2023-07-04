54212b58b35c368b8db0112c587c7248ea9830cc
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-07-04 20:58:15 +0545

    Create [ Lab - 11 ] mid point circle algorithm.cpp

---
+++ Third Semester/Computer Graphics/[ Lab - 11 ] mid point circle algorithm.cpp
@@ -0,0 +1,70 @@
+
+#include <iostream>
+#include <GL/glut.h>
+using namespace std;
+
+int xi, yi, radius;
+
+void plot(int x, int y)
+{
+    glBegin(GL_POINTS);
+    glVertex2i(x + xi, y + yi);
+    glEnd();
+}
+
+void drawCircle()
+{
+    int x = 0;
+    int y = radius;
+    float decision = 5 / 4 - radius;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0, 0.0, 0.0);
+    glPointSize(1.0);
+    plot(x, y);
+
+    while (y > x)
+    {
+        if (decision < 0)
+        {
+            x++;
+            decision += 2 * x + 1;
+        }
+        else
+        {
+            y--;
+            x++;
+            decision += 2 * (x - y) + 1;
+        }
+        plot(x, y);
+        plot(x, -y);
+        plot(-x, y);
+        plot(-x, -y);
+        plot(y, x);
+        plot(-y, x);
+        plot(y, -x);
+        plot(-y, -x);
+    }
+    glFlush();
+}
+
+int main(int argc, char **argv)
+{
+    cout << "Enter the coordinates of the center (x,y): ";
+    cin >> xi >> yi;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 150);
+    glutCreateWindow("Line Drawing Alogrithms");
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glPointSize(4.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 800, 0, 600);
+    glutDisplayFunc(drawCircle);
+    glutMainLoop();
+}
