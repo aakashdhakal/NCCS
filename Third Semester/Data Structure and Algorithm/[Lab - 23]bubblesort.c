e462d1acb214105d2eef2260b2c7e86abb72a02e
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-07-15 21:46:53 +0545

    Update [Lab - 23]bubblesort.c

--- Third Semester/Data Structure and Algorithm/[Lab - 23]bubblesort.c
+++ Third Semester/Data Structure and Algorithm/[Lab - 23]bubblesort.c
@@ -1,36 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void bubbleSort(int number[], int n){
-	int i,j,temp;
-	for(i=0;i<n-1;i++){
-		for(j=i;j<n-1-i;j++){
-			if(number[j]>number[j+1]){
-				temp = number[j];
-				number[j] = number[j+1];
-				number[j+1] = temp;
-			}
-		}
-	}
-			printf("The numbers after sorting: ");
-		for(i=0;i<n;i++){
-		printf("%d\t",number[i]);
-	}
+void bubbleSort(int number[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            if (number[j] > number[j + 1])
+            {
+                temp = number[j];
+                number[j] = number[j + 1];
+                number[j + 1] = temp;
+            }
+        }
+    }
+    printf("The numbers after sorting: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", number[i]);
+    }
 }
 
-int main(){
-	int n;
-	printf("Enter the total no. of elements: ");
-	scanf("%d",&n);
-	int numbers[n],i;
-	printf("Enter %d numbers\n ");
-	for(i=0;i<n;i++){
-		scanf("%d",&numbers[i]);
-	}
-	printf("The numbers before sorting: ");
-	for(i=0;i<n;i++){
-		printf("%d\t",numbers[i]);
-	}
-	printf("\n");
-	bubbleSort(numbers,n);
-
+int main()
+{
+    int n;
+    printf("Enter the total no. of elements: ");
+    scanf("%d", &n);
+    int numbers[n], i;
+    printf("Enter %d numbers\n ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &numbers[i]);
+    }
+    printf("The numbers before sorting: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", numbers[i]);
+    }
+    printf("\n");
+    bubbleSort(numbers, n);
+    printf("\n");
 }
