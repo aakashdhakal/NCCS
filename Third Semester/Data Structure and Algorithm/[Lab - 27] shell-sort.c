729f0f69ca4a51faa8265ee08663e9931ca50166
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-07-18 20:50:17 +0545

    Create [Lab - 27] shell-sort.c

---
+++ Third Semester/Data Structure and Algorithm/[Lab - 27] shell-sort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void shellSort(int *arr, int n)
+{
+    int gap, i, j, temp;
+    for (gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (i = gap; i < n; i++)
+        {
+            temp = arr[i];
+            for (j = i; j >= gap; j -= gap)
+            {
+                if (temp < arr[j - gap])
+                {
+                    arr[j] = arr[j - gap];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the total no. of elements: ");
+    scanf("%d", &n);
+    int numbers[n], i;
+    printf("Enter %d numbers\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &numbers[i]);
+    }
+    printf("The numbers before sorting: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", numbers[i]);
+    }
+    printf("\n");
+    shellSort(numbers, n);
+    printf("The numbers after sorting using shell sort: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", numbers[i]);
+    }
+    printf("\n");
+}
