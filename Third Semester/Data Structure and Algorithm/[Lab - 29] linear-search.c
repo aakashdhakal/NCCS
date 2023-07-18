2fd5486c42edb8804da9683bc811f4d7c1011121
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-07-18 20:53:00 +0545

    Create [Lab - 29] linear-search.c

---
+++ Third Semester/Data Structure and Algorithm/[Lab - 29] linear-search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void linearSearch(int *arr, int key, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (key == arr[i])
+        {
+            printf("\nThe key %d is found in index %d\n", key, i);
+            return;
+        }
+    }
+    printf("\nThe key %d is not found in the array\n", key);
+}
+
+int main()
+{
+    int n;
+    printf("Enter the total no. of elements: ");
+    scanf("%d", &n);
+    int numbers[n], i;
+    printf("Enter %d numbers\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &numbers[i]);
+    }
+    int key;
+    printf("Enter the key to search: ");
+    scanf("%d", &key);
+    linearSearch(numbers, key, n);
+}
