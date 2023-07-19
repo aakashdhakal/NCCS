fc409da5a38b9bc00992c17b2879fe709de3e881
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-07-19 19:14:25 +0545

    Update trapezoidal rule.c

--- Third Semester/Numerical Method/trapezoidal rule.c
+++ Third Semester/Numerical Method/trapezoidal rule.c
@@ -1,36 +1,37 @@
 
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
 
 // यो भित्र को function कृपया change गर्दिनु होला !! धन्यबाद !!
 
 double f(double x)
-	return pow(x,2)+ 2*x;
+{
+    return pow(x, 2) + 2 * x;
 }
 
 int main()
 {
- float lower, upper, integration=0.0, stepSize, k;
- int i, subInterval;
+    float lower, upper, integration = 0.0, stepSize, k;
+    int i, subInterval;
 
- printf("Enter lower limit of integration: ");
- scanf("%f", &lower);
- printf("Enter upper limit of integration: ");
- scanf("%f", &upper);
- printf("Enter number of sub intervals: ");
- scanf("%d", &subInterval);
+    printf("Enter lower limit of integration: ");
+    scanf("%f", &lower);
+    printf("Enter upper limit of integration: ");
+    scanf("%f", &upper);
+    printf("Enter number of sub intervals: ");
+    scanf("%d", &subInterval);
 
- stepSize = (upper - lower)/subInterval;
+    stepSize = (upper - lower) / subInterval;
 
- integration = f(lower) + f(upper);
- for(i=1; i<= subInterval-1; i++)
- {
-  k = lower + i*stepSize;
-  integration = integration + 2 * f(k);
- }
- integration = integration * stepSize/2;
- printf("\nRequired value of integration is: %.3f", integration);
- getch();
- return 0;
+    integration = f(lower) + f(upper);
+    for (i = 1; i <= subInterval - 1; i++)
+    {
+        k = lower + i * stepSize;
+        integration = integration + 2 * f(k);
+    }
+    integration = integration * stepSize / 2;
+    printf("\nRequired value of integration is: %.3f", integration);
+    getch();
+    return 0;
 }
