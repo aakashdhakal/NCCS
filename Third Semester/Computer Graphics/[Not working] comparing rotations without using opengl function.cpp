570396e1fa968d8ce8dada87e6262d6389ae0580
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-08-08 16:13:17 +0545

    Update [Not working] comparing rotations without using opengl function.cpp

--- Third Semester/Computer Graphics/[Not working] comparing rotations without using opengl function.cpp
+++ Third Semester/Computer Graphics/[Not working] comparing rotations without using opengl function.cpp
@@ -1,57 +1,88 @@
-
 #include <GL/glut.h>
 #include <cmath>
+#include <iostream>
+using namespace std;
 
-int x, y;
-int width, height;
+float x, y;
+float width, height;
 int win_width = 800;
 int win_height = 600;
 
-void drawRectangle()
+void rotate()
 {
-    glColor3f(0.0, 0.0, 0.0); // Black color
+    float theta = (30 * 3.14159265) / 180.0;
+    float rotationMatrix[3][3] = {cos(theta), -sin(theta), 0, sin(theta), cos(theta), 0, 0, 0, 1};
+    float translateToOrigin[3][3] = {1, 0, -150,0,1,-125,0,0,1};
+    float translateFromOrigin[3][3] = {1, 0, 150,0,1,125,0,0,1};
+    float originalVertex[3][4] = {{x, x + width, x + width, x}, {y, y, y + height, y + height}, {1, 1, 1, 1}};
+    float tempVertex1[3][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    float tempVertex2[3][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    float finalVertex[3][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glLineWidth(2.0);
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 4; i++)
+    {
+        glVertex2f(originalVertex[0][i], originalVertex[1][i]);
+    }
+    glEnd();
+    glFlush();
+for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                tempVertex1[i][j] += translateToOrigin[i][k] * rotationMatrix[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                tempVertex2[i][j] += tempVertex1[i][k] * translateFromOrigin[k][j];
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                finalVertex[i][j] += tempVertex2[i][k] * originalVertex[k][j];
+            }
+        }
+    }
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 4; i++)
+    {
+        glVertex2f(finalVertex[0][i], finalVertex[1][i]);
+    }
+
+    glEnd();
+    glFlush();
+
+}
+
+void display()
+{
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0, 0.0, 0.0); // Black color
     glBegin(GL_LINES);
     glVertex2i(-800, 0);
     glVertex2i(800, 0);
     glVertex2i(0, -600);
     glVertex2i(0, 600);
     glEnd();
-
-    // Original rectangle
-    glColor3f(1.0, 0.0, 0.0); // Red color
-    glBegin(GL_POLYGON);
-    glVertex2f(x, y);
-    glVertex2f(x + width, y);
-    glVertex2f(x + width, y + height);
-    glVertex2f(x, y + height);
-    glEnd();
-
-    // Rotated Rectangle
-    float x1, y1, x2, y2, x3, y3, x4, y4;
-    float theta = (30 * 3.14159265) / 180.0;
-    float tx = 150.0f;
-    float ty = 125.0f;
-
-    x += tx;
-    y += ty;
-    x1 = x * cos(theta) - y * sin(theta);
-    y1 = x * sin(theta) + y * cos(theta);
-    x2 = (x + width) * cos(theta) - y * sin(theta);
-    y2 = (x + width) * sin(theta) + y * cos(theta);
-    x3 = (x + width) * cos(theta) - (y + height) * sin(theta);
-    y3 = (x + width) * sin(theta) + (y + height) * cos(theta);
-    x4 = x * cos(theta) - (y + height) * sin(theta);
-    y4 = x * sin(theta) + (y + height) * cos(theta);
-    glColor3f(0.0, 0.0, 1.0); // Blue color
-    glBegin(GL_POLYGON);
-    glVertex2f(x1, y1);
-    glVertex2f(x2, y2);
-    glVertex2f(x3, y3);
-    glVertex2f(x4, y4);
-    glEnd();
-
     glFlush();
+    rotate();
 }
 
 int main(int argc, char *argv[])
@@ -65,10 +96,10 @@ int main(int argc, char *argv[])
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutInitWindowSize(win_width, win_height);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Rotate Rectangle - Aakash Dhakal");
+    glutCreateWindow("Rotate Rectangle Without Using Function - Aakash Dhakal");
     glClearColor(1.0, 1.0, 1.0, 1.0);
     gluOrtho2D(-100, win_width, -100, win_height);
-    glutDisplayFunc(drawRectangle);
+    glutDisplayFunc(display);
     glutMainLoop();
 
     return 0;
