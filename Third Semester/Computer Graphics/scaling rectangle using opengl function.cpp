9ee39260cace2d635f65a5e1fec137bb817990b6
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-08-14 22:58:10 +0545

    Update scaling rectangle using opengl function.cpp

--- Third Semester/Computer Graphics/scaling rectangle using opengl function.cpp
+++ Third Semester/Computer Graphics/scaling rectangle using opengl function.cpp
@@ -1,33 +1,53 @@
-
 #include <GL/glut.h>
+#include <cmath>
+#include <iostream>
+using namespace std;
 
-int x, y;
-int width, height;
+float x, y;
+float width, height;
+int win_width = 800;
+int win_height = 600;
 
-void drawRectangle()
+void drawRectangle(float rectangleMatrix[][4])
+{
+    glLineWidth(3.0);
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 4; i++)
+    {
+        glVertex2f(rectangleMatrix[0][i], rectangleMatrix[1][i]);
+    }
+    glEnd();
+    glFlush();
+}
+
+void scale()
+{
+    float sx = 2, sy = 2;
+    float theta = (30 * 3.14159265) / 180.0;
+    float scaleMatrix[3][3] = {sx, 0, 0, 0, sy, 0, 0, 0, 1};
+    float originalVertex[3][4] = {{x, x + width, x + width, x}, {y, y, y + height, y + height}, {1, 1, 1, 1}};
+
+    glColor3f(0.0f, 0.0f, 0.0f);
+    drawRectangle(originalVertex);
+
+    glScalef(sx, sy, 1.0);
+
+    glColor3f(0.5f, 0.5f, 0.5f);
+    drawRectangle(originalVertex);
+}
+
+void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    // Original rectangle
-    glColor3f(1.0, 0.0, 0.0); // Red color
-    glBegin(GL_POLYGON);
-    glVertex2f(x, y);
-    glVertex2f(x + width, y);
-    glVertex2f(x + width, y + height);
-    glVertex2f(x, y + height);
+    glColor3f(0.0, 0.0, 0.0); // Black color
+    glBegin(GL_LINES);
+    glVertex2i(-800, 0);
+    glVertex2i(800, 0);
+    glVertex2i(0, -600);
+    glVertex2i(0, 600);
     glEnd();
-
-    //Rotated Rectangle
-    glScalef(2.0f,2.0f,0.0f);
-    glColor3f(0.0, 0.0, 1.0); // Blue color
-    glBegin(GL_POLYGON);
-    glVertex2f(x, y);
-    glVertex2f(x + width, y);
-    glVertex2f(x + width, y + height);
-    glVertex2f(x, y + height);
-    glEnd();
-
     glFlush();
+    scale();
 }
 
 int main(int argc, char *argv[])
@@ -39,12 +59,12 @@ int main(int argc, char *argv[])
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowSize(800, 600);
+    glutInitWindowSize(win_width, win_height);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Rotate Rectangle - Aakash Dhakal");
+    glutCreateWindow("Rotate Rectangle Without Using Function - Aakash Dhakal");
     glClearColor(1.0, 1.0, 1.0, 1.0);
-    gluOrtho2D(0, 800, 0, 600);
-    glutDisplayFunc(drawRectangle);
+    gluOrtho2D(-100, win_width, -100, win_height);
+    glutDisplayFunc(display);
     glutMainLoop();
 
     return 0;
