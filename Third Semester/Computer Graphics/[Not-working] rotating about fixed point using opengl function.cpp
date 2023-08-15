af2abb47573008e4ffe0975899812dd4310f8876
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-08-15 15:54:41 +0545

    Update and rename [Not-working] comparing rotaions.cpp to [Not-working] rotating about fixed point using opengl function.cpp

---
+++ Third Semester/Computer Graphics/[Not-working] rotating about fixed point using opengl function.cpp
@@ -0,0 +1,71 @@
+#include <GL/glut.h>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+float x, y;
+float width, height;
+int win_width = 800;
+int win_height = 600;
+
+void drawRectangle(float rectangle[][4]){
+    glLineWidth(2.0);
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 4; i++)
+    {
+        glVertex2f(rectangle[0][i], rectangle[1][i]);
+    }
+    glEnd();
+    glFlush();
+}
+
+void rotate()
+{
+    float theta = (30 * 3.14159265) / 180.0;
+    float rotationMatrix[3][3] = {cos(theta), -sin(theta), 0, sin(theta), cos(theta), 0, 0, 0, 1};
+    float originalVertex[3][4] = {{x, x + width, x + width, x}, {y, y, y + height, y + height}, {1, 1, 1, 1}};
+
+    glColor3f(0.0f, 0.0f, 0.0f);
+    drawRectangle(originalVertex);
+
+    glTranslatef(-150,-125,0);
+    glRotatef(30,0,0,1);
+    glTranslatef(150,125,0);
+
+    glColor3f(0.5, 0.5, 0.5);
+    drawRectangle(originalVertex);
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0, 0.0, 0.0); // Black color
+    glBegin(GL_LINES);
+    glVertex2i(-800, 0);
+    glVertex2i(800, 0);
+    glVertex2i(0, -600);
+    glVertex2i(0, 600);
+    glEnd();
+    glFlush();
+    rotate();
+}
+
+int main(int argc, char *argv[])
+{
+    x = 100;
+    y = 100;
+    width = 100;
+    height = 50;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowSize(win_width, win_height);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Rotate Rectangle Without Using Function - Aakash Dhakal");
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    gluOrtho2D(-100, win_width, -100, win_height);
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
