b818ef0dafb5d4d8b1b0d73e8b1cda37b4a4bcbb
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-08-15 15:51:31 +0545

    Update and rename [Not Correct] scaling rectangle without using opengl function.cpp to [ Lab - 15 ] scaling rectangle without using opengl function.cpp

--- Third Semester/Computer Graphics/[ Lab - 15 ] scaling rectangle without using opengl function.cpp
+++ Third Semester/Computer Graphics/[ Lab - 15 ] scaling rectangle without using opengl function.cpp
@@ -1,6 +1,5 @@
 #include <GL/glut.h>
 #include <cmath>
-#include <iostream>
 using namespace std;
 
 float x, y;
@@ -8,24 +7,29 @@ float width, height;
 int win_width = 800;
 int win_height = 600;
 
-void scale()
-{
-    float sx = 2, sy = 2;
-    float theta = (30 * 3.14159265) / 180.0;
-    float scaleMatrix[3][3] = {sx, 0, 0, 0, sy, 0, 0, 0, 1};
-    float originalVertex[3][4] = {{x, x + width, x + width, x}, {y, y, y + height, y + height}, {1, 1, 1, 1}};
-
-    float finalVertex[3][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-    glColor3f(0.0f, 0.0f, 1.0f);
+void drawRectangle(float rectangle[][4]){
     glLineWidth(2.0);
     glBegin(GL_LINE_LOOP);
     for (int i = 0; i < 4; i++)
     {
-        glVertex2f(originalVertex[0][i], originalVertex[1][i]);
+        glVertex2f(rectangle[0][i], rectangle[1][i]);
     }
     glEnd();
     glFlush();
+}
+
+void scale()
+{
+    float theta = (30 * 3.14159265) / 180.0;
+    float sx = 2, sy = 2;
+    float scaleMatrix[3][3] = {sx,0,0, 0,sy,0, 0, 0, 1};
+    float originalVertex[3][4] = {{x, x + width, x + width, x}, {y, y, y + height, y + height}, {1, 1, 1, 1}};
+
+    float finalVertex[3][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    glColor3f(0.0f, 0.0f, 0.0f);
+    drawRectangle(originalVertex);
+
 
     for (int i = 0; i < 3; i++)
     {
@@ -37,15 +41,8 @@ void scale()
             }
         }
     }
-    glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_LINE_LOOP);
-    for (int i = 0; i < 4; i++)
-    {
-        glVertex2f(finalVertex[0][i], finalVertex[1][i]);
-    }
-
-    glEnd();
-    glFlush();
+    glColor3f(0.5, 0.5, 0.5);
+    drawRectangle(finalVertex);
 }
 
 void display()
