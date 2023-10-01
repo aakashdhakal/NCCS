8a448a20bc9c9a802175c566dde0e3b1b704922e
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-10-01 10:18:42 +0545

    Update Fixed Point Method.c

--- Third Semester/Numerical Method/Fixed Point Method.c
+++ Third Semester/Numerical Method/Fixed Point Method.c
@@ -6,36 +6,36 @@
 // यो भित्र को function कृपया change गर्दिनु होला !! धन्यबाद !!
 float f(float x)
 {
-	return (sin(x) - 5 * x + 2);
+    return x * exp(x) - 1;
 }
 
 float g(float x)
 {
-	return ((sin(x) + 2) / 5);
+    return 1 / exp(x);
 }
 
 int main()
 {
-	int step = 1;
-	float x0, x1, e;
-	printf("Enter initial guess: ");
-	scanf("%f", &x0);
-	printf("Enter tolerable error: ");
-	scanf("%f", &e);
+    int step = 1;
+    float x0, x1, e, E;
+    printf("Enter initial guess: ");
+    scanf("%f", &x0);
+    printf("Enter tolerable error: ");
+    scanf("%f", &e);
 
-	printf("\n\tStep\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
-	do
-	{
-		x1 = g(x0);
-		printf("\t%d\t%f\t%f\t%f\t%f\n", step, x0, f(x0), x1, f(x1));
+    printf("\n\tStep\t  x0\t\t  x1\n");
+    do
+    {
+        x1 = g(x0);
+        printf("\t%d\t%f\t%f\n", step, x0, x1);
+        E = fabs((x1 - x0) / x1);
+        step = step + 1;
+        x0 = x1;
 
-		step = step + 1;
-		x0 = x1;
+    } while (E > e);
 
-	} while (fabs(f(x1)) > e);
+    printf("\nRoot is %f", x1);
 
-	printf("\nRoot is %f", x1);
-
-	getch();
-	return 0;
+    getch();
+    return 0;
 }
