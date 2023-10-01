7e3670d84c942601b7d358a3be66a795343fa4aa
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-10-01 09:42:06 +0545

    Update Secant Method.c

--- Third Semester/Numerical Method/Secant Method.c
+++ Third Semester/Numerical Method/Secant Method.c
@@ -5,51 +5,43 @@
 // यो भित्र को function कृपया change गर्दिनु होला !! धन्यबाद !!
 float f(float x)
 {
-	return (pow(x, 2) - 4 * x - 10);
+    return pow(x - 2, 2) - log10(x);
 }
 
-void main()
+int main()
 {
-	float x0, x1, x2, f0, f1, f2, e;
-	int step = 1, N;
+    float x0, x1, x2, f0, f1, f2, e, E;
+    int step = 1, N;
 
-	printf("\nEnter initial guesses: ");
-	scanf("%f%f", &x0, &x1);
-	printf("Enter tolerable error: ");
-	scanf("%f", &e);
-	printf("Enter maximum iteration: ");
-	scanf("%d", &N);
+    printf("\nEnter initial guesses: ");
+    scanf("%f%f", &x0, &x1);
+    printf("Enter tolerable error: ");
+    scanf("%f", &e);
 
-	printf("\n\tStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
-	do
-	{
-		f0 = f(x0);
-		f1 = f(x1);
-		if (f0 == f1)
-		{
-			printf("Mathematical Error.");
-			return 0;
-		}
+    printf("\n\tStep\t\tx0\t\tx1\t\tf(x0)\t\tf(x1)\t\tx2\n");
+    do
+    {
+        f0 = f(x0);
+        f1 = f(x1);
+        if (f0 == f1)
+        {
+            printf("Mathematical Error.");
+            return 0;
+        }
 
-		x2 = x1 - (x1 - x0) * f1 / (f1 - f0);
-		f2 = f(x2);
+        x2 = (x0 * f1 - x1 * f0) / (f1 - f0);
 
-		printf("\t%d\t\t%f\t%f\t%f\t%f\n", step, x0, x1, x2, f2);
+        E = fabs((x2 - x1) / x2);
 
-		x0 = x1;
-		f0 = f1;
-		x1 = x2;
-		f1 = f2;
+        printf("\t%d\t\t%f\t%f\t%f\t%f\t%f\n", step, x0, x1, f0, f1, x2);
 
-		step = step + 1;
+        x0 = x1;
+        x1 = x2;
 
-		if (step > N)
-		{
-			printf("Not Convergent.");
-			return 0;
-		}
-	} while (fabs(f2) > e);
+        step = step + 1;
 
-	printf("\nRoot is: %f", x2);
-	getch();
+    } while (E > e);
+
+    printf("\nRoot is: %f", x2);
+    getch();
 }
