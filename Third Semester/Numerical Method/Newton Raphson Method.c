856ab55d20b939c178f471df26c66ddde7cd6f83
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-10-01 10:55:46 +0545

    Update Newton Raphson Method.c

--- Third Semester/Numerical Method/Newton Raphson Method.c
+++ Third Semester/Numerical Method/Newton Raphson Method.c
@@ -5,49 +5,49 @@
 // यो भित्र को function कृपया change गर्दिनु होला !! धन्यबाद !!
 float f(float x)
 {
-	return (pow(x, 2) + 2 * x - 2);
+    return x * log10(x) - 1.2;
 }
 
 float g(float x)
 {
-	return (2 * x + 2);
+    return 1 / log(10) + log10(x);
 }
 
 int main()
 {
-	float x0, x1, f0, f1, g0, e;
-	int step = 1;
+    float x0, x1, f0, f1, g0, e, E;
+    int step = 1;
 
-	printf("\nEnter initial guess: ");
-	scanf("%f", &x0);
+    printf("\nEnter initial guess: ");
+    scanf("%f", &x0);
 
-	printf("Enter tolerable error: ");
-	scanf("%f", &e);
+    printf("Enter tolerable error: ");
+    scanf("%f", &e);
 
-	printf("\n\tStep\t\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
+    printf("\n\tStep\t\tx0\t\tf(x0)\t\tg(x0)\t\tx1\n");
 
-	do
-	{
-		g0 = g(x0);
-		f0 = f(x0);
+    do
+    {
+        g0 = g(x0);
+        f0 = f(x0);
 
-		if (g0 == 0.0)
-		{
-			printf("Mathematical Error.");
-			return 0;
-		}
+        if (g0 == 0.0)
+        {
+            printf("Mathematical Error.");
+            return 0;
+        }
 
-		x1 = x0 - f0 / g0;
-		f1 = f(x1);
+        x1 = x0 - f0 / g0;
+        E = fabs((x1 - x0) / x1);
 
-		printf("\t%d\t\t%f\t%f\t%f\t%f\n", step, x0, f0, x1, f1);
+        printf("\t%d\t\t%f\t%f\t%f\t%f\n", step, x0, f0, g0, x1);
 
-		x0 = x1;
-		step = step + 1;
+        x0 = x1;
+        step = step + 1;
 
-	} while (fabs(f1) > e);
+    } while (E > e);
 
-	printf("\nRoot is: %f", x1);
-	getch();
-	return 0;
+    printf("\nRoot is: %f", x1);
+    getch();
+    return 0;
 }
