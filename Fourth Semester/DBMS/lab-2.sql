0f9fe97bc9850f7cb857358a09bdffb15c25496d
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-01-18 12:46:36 +0545

    Add files via upload

--- Fourth Semester/DBMS/lab-2.sql
+++ Fourth Semester/DBMS/lab-2.sql
@@ -14,9 +14,10 @@ INSERT INTO department VALUES(103, "CSIT", 30);
 CREATE TABLE employee(
  eid VARCHAR(10) PRIMARY KEY,
  ename VARCHAR(20),
- address TEXT,
+ address VARCHAR(100),
  salary INT,
- dno INT
+ dno INT,
+ FOREIGN KEY(dno) REFERENCES department(dno)
 );
 /
 INSERT INTO employee VALUES("E01","Bharad","Tokha",15000,103);
@@ -24,3 +25,4 @@ INSERT INTO employee VALUES("E02","Rajib","Bhaktapur",25000,102);
 INSERT INTO employee VALUES("E03","Aakash","Gothatar",150000,101);
 INSERT INTO employee VALUES("E04","Amardeep","Kapan",5000,103);
 /
+SELECT * FROM employee;
