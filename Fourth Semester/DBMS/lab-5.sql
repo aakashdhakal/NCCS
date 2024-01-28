b5b92d3a2e427078e2d6baba768bac7380d0b41e
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-01-28 16:24:51 +0545

    Add files via upload

--- Fourth Semester/DBMS/lab-5.sql
+++ Fourth Semester/DBMS/lab-5.sql
@@ -1,11 +1,11 @@
-DROP TABLE employee;
-/
 CREATE TABLE employee(
-eid INT PRIMARY KEY,
+eid NUMBER PRIMARY KEY,
 ename VARCHAR(20),
 address VARCHAR(20),
 salary INT CHECK(salary BETWEEN 17000 AND 100000)
 );
+/
+DROP TABLE employee
 /
 INSERT INTO employee VALUES(1,"Bharad","Tokha",60000);
 INSERT INTO employee VALUES(2,"Bharad","Tokha",70000);
@@ -16,3 +16,21 @@ INSERT INTO employee VALUES(6,"Bharad","Tokha",865000);
 INSERT INTO employee VALUES(7,"Bharad","Tokha",150000);
 /
 SELECT * FROM employee;
+/
+UPDATE employee
+SET salary = salary*1.1
+WHERE address LIKE 'K%';
+/  
+UPDATE employee
+SET salary = 40000
+WHERE address = 'Pokhara';
+/
+SELECT 
+MAX(salary) AS maximum_salary,
+MIN(salary) AS minimum_salary
+FROM employee;
+/
+SELECT address, AVG(salary) FROM employee GROUP BY address;
+/
+SELECT * FROM employee ORDER BY salary;
+/
