7e2d714d7b82e900650fce2d5b691c29c8fb9031
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-02-20 22:06:21 +0545

    Update [Lab-6]-Dining-Philosopher-problem.c

--- Fourth Semester/OS/[Lab-6]-Dining-Philosopher-problem.c
+++ Fourth Semester/OS/[Lab-6]-Dining-Philosopher-problem.c
@@ -1,75 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void one();
-void two();
-
-int tph, philname[20], status[20], howhung, hu[20], cho;
-
-int main() {
-    int i;
-    printf("\n\n Dining Philosopher Problem");
-    printf("\nEnter the total no. of philosophers:");
-    scanf("%d", &tph);
-    for (i = 0; i < tph; i++) {
-        philname[i] = (i + 1);
-        status[i] = 1;
+int wait(int a)
+{
+    if (a != 0)
+    {
+        return 1;
     }
-    printf("How many are hungry:");
-    scanf("%d", &howhung);
-    if (howhung == tph) {
-        printf("\nAll are hungry..\nDead lock stage will occur");
-        printf("\nExiting\n");
-    } else {
-        for (i = 0; i < howhung; i++) {
-            printf("Enter philosopher %d position:", (i + 1));
-            scanf("%d", &hu[i]);
-        }
-        do {
-            printf("1.One can eat at a time\t2.Two can eat at a time\t3.Exit\nEnter your choice:");
-            scanf("%d", &cho);
-            switch (cho) {
-                case 1:
-                    one();
-                    break;
-                case 2:
-                    two();
-                    break;
-                case 3:
-                    exit(0);
-                default:
-                    printf("\nInvalid option");
+    else
+        return 0;
+}
+
+int main()
+{
+    int n, chopstick[10], count = 0, i, check[10], phil[10];
+    printf("Enter the total number of philosophers: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        chopstick[i] = 1;
+        check[i] = 0;
+        phil[i] = 0;
+    }
+
+    while (count < n)
+    {
+        for (i = 0; i < n; i++)
+        {
+            if (check[i] == 1)
+            {
+                printf("Philosopher %d has finished eating \n", i + 1);
+                check[i] = 0;
+                phil[i] = 1;
+                chopstick[i]++;
+                chopstick[(i + 1) % n]++;
+                count++;
             }
-        } while (1);
-    }
-}
+            if (phil[i] == 0)
+            {
 
-void one() {
-    int pos = 0, x, i;
-    printf("\nAllow one philosopher to eat at any time\n");
-    for (i = 0; i < howhung; i++, pos++) {
-        printf("\nP%d is granted to eat", philname[hu[pos]]);
-        for (x = pos; x < howhung; x++)
-            printf("\nP%d is waiting", philname[hu[x]]);
-    }
-}
-
-void two() {
-    int i, j, s = 0, t, r, x;
-    printf("\nAllow two philosophers to eat at a time\n");
-    for (i = 0; i < howhung; i++) {
-        for (j = i + 1; j < howhung; j++) {
-            if ((abs(hu[i] - hu[j]) == 1) || (abs(hu[i] - hu[j]) == tph - 1)) {
-                printf("\n\nCombination %d\n", (s + 1));
-                t = hu[i];
-                r = hu[j];
-                s++;
-                printf("\nP%d and P%d are granted to eat", philname[hu[i]], philname[hu[j]]);
-                for (x = 0; x < howhung; x++) {
-                    if ((hu[x] != t) && (hu[x] != r))
-                        printf("\nP%d is waiting", philname[hu[x]]);
+                if (wait(chopstick[i]) && wait(chopstick[(i + 1) % n]))
+                {
+                    chopstick[i]--;
+                    chopstick[(i + 1) % n]--;
+                    printf("Philosopher %d is eating\n", i + 1);
+                    check[i] = 1;
+                }
+                else
+                {
+                    printf("Philosopher %d is waiting\n", i + 1);
                 }
             }
         }
     }
+    return 0;
 }
