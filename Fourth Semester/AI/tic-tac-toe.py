b4a127c3cc13cc4e6bfa98641554b2cf15af4c82
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-03-11 00:55:56 +0545

    Update tic-tac-toe.py

--- Fourth Semester/AI/tic-tac-toe.py
+++ Fourth Semester/AI/tic-tac-toe.py
@@ -1,95 +1,52 @@
-import time
 import os
-     
-board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']     
-player = 1     
-    
-########win Flags##########     
-Win = 1     
-Draw = -1     
-Running = 0     
-Stop = 1     
-###########################     
-Game = Running     
-Mark = 'X'     
-    
-#This Function Draws Game Board     
-def DrawBoard():
-    print(" %c | %c | %c " % (board[1],board[2],board[3]))
-    print("___|___|___")     
-    print(" %c | %c | %c " % (board[4],board[5],board[6]))
-    print("___|___|___")     
-    print(" %c | %c | %c " % (board[7],board[8],board[9]))
-    print("   |   |   ")     
-    
-#This Function Checks position is empty or not
-def CheckPosition(x):
-    if(board[x] == ' '):
-        return True
-    else:
-        return False     
-    
-#This Function Checks player has won or not
-def CheckWin():
-    global Game     
-    #Horizontal winning condition
-    if(board[1] == board[2] and board[2] == board[3] and board[1] != ' '):
-        Game = Win
-    elif(board[4] == board[5] and board[5] == board[6] and board[4] != ' '):
-        Game = Win
-    elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):
-        Game = Win     
 
-    #Vertical Winning Condition
-    elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):
-        Game = Win
+board = [' '] * 10
+player = 1
+winning_conditions = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7))
+winner = None
 
-    elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):
-        Game = Win
-    elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):
-        Game=Win     
+def print_board():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(" %c | %c | %c " % (board[1], board[2], board[3]))
+    print("___|___|___")
+    print(" %c | %c | %c " % (board[4], board[5], board[6]))
+    print("___|___|___")
+    print(" %c | %c | %c " % (board[7], board[8], board[9]))
+    print("   |   |   ")
 
-    #Diagonal Winning Condition
-    elif(board[1] == board[5] and board[5] == board[9] and board[5] != ' '):
-        Game = Win
-    elif(board[3] == board[5] and board[5] == board[7] and board[5] != ' '):
-        Game=Win     
+def check_win():
+    global winner
+    for row in winning_conditions:
+        if all(board[i] == board[row[0]] for i in row) and board[row[0]] != ' ':
+            winner = board[row[0]]
+            return
+    if all(x != ' ' for x in board):
+        winner = 'Draw'
 
-    #Match Tie or Draw Condition
-    elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):
-        Game=Draw
-    else:             
-        Game=Running     
-     
-print("Tic-Tac-Toe Game")
-print("Player 1 [X] --- Player 2 [O]\n")
-print()
-print()     
-print("Please Wait...")
-time.sleep(1)
-while(Game == Running):
-    os.system('cls')
-    DrawBoard()
-    if(player % 2 != 0):
-        print("Player 1's chance")
-        Mark = 'X'
-    else:
-        print("Player 2's chance")
-        Mark = 'O'
+def turn():
+    global player
+    mark = 'X' if player % 2 == 1 else 'O'
+    print(f"Player {player}'s turn ({mark})")
+    while True:
+        choice = int(input("Enter a position (1-9): "))
+        if board[choice] == ' ':
+            board[choice] = mark
+            player = 1 if player == 2 else 2
+            return
 
-    choice = int(input("Enter the position between [1-9] where you want to mark : "))     
-    if(CheckPosition(choice)):
-                       board[choice] = Mark
-                       player+=1
-                       CheckWin()
-os.system('cls')
-DrawBoard()
-if(Game==Draw):
-    print("Game Draw")
-elif(Game==Win):
-    player-=1
+print("Tic-Tac-Toe")
+print("Player 1 (X) - Player 2 (O)")
 
-if(player%2!=0):
-    print("Player 1 Won")
-else:
-    print("Player 2 Won")     
+while not winner:
+    print_board()
+    turn()
+    check_win()
+
+print_board()
+
+if winner == 'X':
+    print("Player 1 Wins!")
+elif winner == 'O':
+    print("Player 2 Wins!")
+elif winner == 'Draw':
+    print("Game Drawn!")
