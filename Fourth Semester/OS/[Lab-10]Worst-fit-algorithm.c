091c62b1e38b33a3d51a7422d6878cbd1f8c4b7a
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-03-11 16:59:37 +0545

    Update [Lab-10]Worst-fit-algorithm.c

--- Fourth Semester/OS/[Lab-10]Worst-fit-algorithm.c
+++ Fourth Semester/OS/[Lab-10]Worst-fit-algorithm.c
@@ -1,61 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int main() {
-    int i, j, k, n1, n2, count;
-    int block[10], process[10], diff[10], mem[10];
-
+int main()
+{
+    int i, j, block[10], process[10], n1, n2, max, maxIndex;
     printf("Enter the number of memory blocks: ");
     scanf("%d", &n1);
-    for (i = 0; i < n1; i++) {
+    for (i = 0; i < n1; i++)
+    {
         printf("Enter memory block %d: ", i + 1);
         scanf("%d", &block[i]);
     }
-
     printf("Enter the number of processes: ");
     scanf("%d", &n2);
-    for (i = 0; i < n2; i++) {
+    for (i = 0; i < n2; i++)
+    {
         printf("Enter the memory required by process %d: ", i + 1);
         scanf("%d", &process[i]);
     }
-
     printf("Process\tProcess Size\tMemory Block\n");
-    for (i = 0; i < n2; i++) {
-        count = 0;
-        for (j = 0; j < n1; j++) {
-            if (process[i] <= block[j]) {
-                diff[count] = block[j] - process[i];  // Calculate remaining space
-                mem[count] = j;                         // Store memory block index
-                count++;
+    for (i = 0; i < n2; i++)
+    {
+        max = -1;
+        for (j = 0; j < n1; j++)
+        {
+            if (process[i] <= block[j] && (max == -1 || block[j] - process[i] > max))
+            {
+                max = block[j] - process[i];
+                maxIndex = j;
             }
         }
-
-        if (count == 0) {
+        if (max != -1)
+        {
+            block[maxIndex] -= process[i];
+            printf("%d\t\t%d\t\t%d\n", i + 1, process[i], maxIndex + 1);
+        }
+        else
+        {
             printf("%d\t\t%d\t\tNot allocated\n", i + 1, process[i]);
-        } else if (count == 1) {
-            block[mem[0]] -= process[i];  // Update memory block size
-            printf("%d\t\t%d\t\t%d\n", i + 1, process[i], mem[0] + 1);
-        } else {
-            // Sort diffs and corresponding memory block indices in **ascending order**
-            // (smallest remaining space comes first)
-            for (j = 0; j < count - 1; j++) {
-                for (k = j + 1; k < count; k++) {
-                    if (diff[j] < diff[k]) {
-                        swap(&diff[j], &diff[k]);
-                        swap(&mem[j], &mem[k]);
-                    }
-                }
-            }
-            block[mem[0]] -= process[i];  // Update memory block size after allocation
-            printf("%d\t\t%d\t\t%d\n", i + 1, process[i], mem[0] + 1);
         }
     }
-
     return 0;
 }
