c12a6604af971ed7328d667c4eb1306ae152d554
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-05-01 22:35:13 +0545

    Update 5.PDA-that-accepts-a^n-b^n.cpp

--- Fourth Semester/Theory of Computation/5.PDA-that-accepts-a^n-b^n.cpp
+++ Fourth Semester/Theory of Computation/5.PDA-that-accepts-a^n-b^n.cpp
@@ -1,79 +1,46 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+
 using namespace std;
 
-class Stack
-{
-    int top;
-    char arr[100];
+struct stack{
+    char A[10];
+    int top=-1;
+}S;
 
-public:
-    Stack()
-    {
-        top = -1;
-    }
-    void push(char x)
-    {
-        arr[++top] = x;
-    }
-    void pop()
-    {
-        if (!isEmpty())
-            top--;
-    }
-    bool isEmpty()
-    {
-        return top == -1;
-    }
-    char peek()
-    {
-        if (!isEmpty())
-            return arr[top];
-        else
-            return ' ';
-    }
-};
+void push(char a){
+    S.A[++S.top]=a;
+}
+void pop(){
+    S.top--;
+}
 
-int main()
-{
+int main(){
     string str;
-    cout << "Enter the string: ";
-    cin >> str;
-    Stack s;
-    int i = 0;
-
-    while (str[i] != '\0')
-    {
-        if (str[i] == '0' && (s.isEmpty() || s.peek() == '0'))
-        {
-            s.push('0');
-        }
-        else if (str[i] == '1' && (s.isEmpty() || s.peek() == '1'))
-        {
-            s.push('1');
-        }
-        else if (str[i] == '0' && s.peek() == '1')
-        {
-            s.pop();
-        }
-        else if (str[i] == '1' && s.peek() == '0')
-        {
-            s.pop();
-        }
-        else
-        {
-            cout << "Not Accepted" << endl;
+    int len,state=0;
+    cout<<"Enter a string :";
+    cin>>str;
+    len=str.length();
+    for(int i=0;i<len;i++){
+        if(state==0 && str[i]=='a' &&S.A[S.top]=='a'){
+            push('a');
+            state=0;}
+        else if(state==0 && str[i]=='a' &&S.top==-1){
+            push('a');
+            state=0;}
+        else if(state==0 && str[i]=='b' &&S.A[S.top]=='a'){
+            pop();
+            state=1;}
+        else if(state==1 && str[i]=='b' && S.A[S.top]=='a'){
+            pop();
+            state=1;}
+        else{
+            cout<<"String rejected";
             return 0;
         }
-        i++;
-    }
-    if (s.isEmpty())
-    {
-        cout << "Accepted" << endl;
-    }
+        }
+    
+    if(S.top==-1)
+        cout<<"String accepted";
     else
-    {
-        cout << "No Accepted" << endl;
-    }
-    return 0;
+        cout<<"String rejected";
 }
