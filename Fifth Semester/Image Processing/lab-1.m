b5c5a4e5498093b1f930938a02e583f2fdbb489e
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-05-17 13:39:56 +0545

    Update lab-1.m

--- Fifth Semester/Image Processing/lab-1.m
+++ Fifth Semester/Image Processing/lab-1.m
@@ -1,1 +1,12 @@
-I
+clc
+close all
+clear all
+A= imread('Batman.png');
+subplot(1,2,1)
+imshow(A)
+title('Color Image of Batman')
+B=rgb2gray(A);
+#figure
+subplot(1,2,2)
+imshow(B)
+title('GrayScale Image of Batman')
