915847c84a70698522183e020a1165c0c4ecc1a9
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-06-07 14:19:06 +0545

    Create lab-4.m

---
+++ Fifth Semester/Image Processing/lab-4.m
@@ -0,0 +1,34 @@
+clear all;
+close all;
+clc;
+
+a=imread('penguin.jpg');
+subplot(2,2,1);
+imshow(a);
+title 'Original Image';
+b=im2double(a);
+
+a1 = input('Enter the value of a: ');
+ga1 = input('Enter the gamma value: ');
+s = (a1*(b.^ga1))*256;
+s1=uint8(s);
+subplot(2,2,2);
+imshow(s1);
+title "Image 1";
+
+a2 = input('Enter the value of a: ');
+ga2 = input('Enter the gamma value: ');
+sp = (a2*(b.^ga2))*256;
+s2=uint8(sp);
+subplot(2,2,3);
+imshow(s2);
+title "Image 2";
+
+a3 = input('Enter the value of a: ');
+ga3 = input('Enter the gamma value: ');
+sp2 = (a3*(b.^ga3))*256;
+s3=uint8(sp2);
+subplot(2,2,4);
+imshow(s1);
+title "Image 1";
+
