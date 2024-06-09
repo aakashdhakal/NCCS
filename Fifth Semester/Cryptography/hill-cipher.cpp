550ba7a4973ab767856202604019b1ae1cb0a4ea
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-06-09 21:28:45 +0545

    Refactor Hill Cipher encryption and decryption functions

--- Fifth Semester/Cryptography/hill-cipher.cpp
+++ Fifth Semester/Cryptography/hill-cipher.cpp
@@ -1,89 +1,83 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
+#include <cstdlib>
+
 using namespace std;
-float en[3][1], de[3][1], a[3][3], b[3][3], msg[3][1], m[3][3];
-void getKeyMatrix()
-{ // get key and message from user
+
+float encrypt[2][1] = {{0}, {0}}, decrypt[2][1] = {{0}, {0}}, a[2][2], b[2][2] = {{0}}, mes[2][1] = {{0}}, c[2][2] = {{0}};
+void inverse()
+{
     int i, j;
-    char mes[3];
-    cout << "Enter 3x3 matrix for key (should have inverse):\n";
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
+    float det = 0;
+
+    // Calculate the determinant
+    det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+
+    if (det == 0)
+    {
+        cout << "The key matrix is not invertible.\n";
+        exit(1);
+    }
+
+    // Calculate the inverse matrix
+    float invdet = 1.0 / det;
+    b[0][0] = a[1][1] * invdet;
+    b[0][1] = -a[0][1] * invdet;
+    b[1][0] = -a[1][0] * invdet;
+    b[1][1] = a[0][0] * invdet;
+}
+
+void encryption()
+{
+    int i, j, k;
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 1; j++)
+            for (k = 0; k < 2; k++)
+                encrypt[i][j] += a[i][k] * mes[k][j];
+
+    cout << "\nEncrypted string is: ";
+    for (i = 0; i < 2; i++)
+        cout << (char)(fmod(encrypt[i][0], 26) + 97);
+}
+
+void decryption()
+{
+    int i, j, k;
+    inverse();
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 1; j++)
+            for (k = 0; k < 2; k++)
+                decrypt[i][j] += b[i][k] * encrypt[k][j];
+
+    cout << "\nDecrypted string is: ";
+    for (i = 0; i < 2; i++)
+        cout << (char)(fmod(decrypt[i][0], 26) + 97);
+    cout << endl;
+}
+
+void getKeyMessage()
+{
+    int i, j;
+    char msg[2];
+
+    cout << "Enter 2x2 matrix for key (It should be invertible):\n";
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++)
         {
             cin >> a[i][j];
-            m[i][j] = a[i][j];
+            c[i][j] = a[i][j];
         }
-    cout << "\nEnter a string of 3 letter(use A through Z): ";
-    cin >> mes;
-    for (i = 0; i < 3; i++)
-        msg[i][0] = mes[i] - 65;
+
+    cout << "\nEnter a 2 letter string: ";
+    cin >> msg;
+    for (i = 0; i < 2; i++)
+        mes[i][0] = msg[i] - 97;
 }
-void encrypt()
-{ // encrypts the message
-    int i, j, k;
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 1; j++)
-            for (k = 0; k < 3; k++)
-                en[i][j] = en[i][j] + a[i][k] * msg[k][j];
-    cout << "\nEncrypted string is: ";
-    for (i = 0; i < 3; i++)
-        cout << (char)(fmod(en[i][0], 26) + 65); // modulo 26 is taken for each element of the matrix obtained by multiplication
-}
-void inversematrix()
-{ // find inverse of key matrix
-    int i, j, k;
-    float p, q;
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-        {
-            if (i == j)
-                b[i][j] = 1;
-            else
-                b[i][j] = 0;
-        }
-    for (k = 0; k < 3; k++)
-    {
-        for (i = 0; i < 3; i++)
-        {
-            p = m[i][k];
-            q = m[k][k];
-            for (j = 0; j < 3; j++)
-            {
-                if (i != k)
-                {
-                    m[i][j] = m[i][j] * q - p * m[k][j];
-                    b[i][j] = b[i][j] * q - p * b[k][j];
-                }
-            }
-        }
-    }
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            b[i][j] = b[i][j] / m[i][i];
-    cout << "\n\nInverse Matrix is:\n";
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-            cout << b[i][j] << " ";
-        cout << "\n";
-    }
-}
-void decrypt()
-{ // decrypt the message
-    int i, j, k;
-    inversematrix();
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 1; j++)
-            for (k = 0; k < 3; k++)
-                de[i][j] = de[i][j] + b[i][k] * en[k][j];
-    cout << "\nDecrypted string is: ";
-    for (i = 0; i < 3; i++)
-        cout << (char)(fmod(de[i][0], 26) + 65); // modulo 26 is taken to get the original message
-    cout << "\n";
-}
+
 int main()
 {
-    getKeyMatrix();
-    encrypt();
-    decrypt();
+    getKeyMessage();
+    encryption();
+    decryption();
+    return 0;
 }
