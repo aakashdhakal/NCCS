c8705f38f84bd2278c2c1cc9611a12ad427a1ce6
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-07-12 14:24:21 +0545

    Create lab-7.m

---
+++ Fifth Semester/Image Processing/lab-7.m
@@ -0,0 +1,21 @@
+clear all;
+close all;
+clc;
+
+pkg load image;
+
+i=imread("Batman.jpg");
+subplot(4,2,1);imshow(i);title("Original Image");
+g = rgb2gray(i);
+subplot(4,2,5);imshow(i);title("Gray Image");
+j = imadjust(g,[0.3,0.7],[]);
+subplot(4,2,3);imshow(i);title("Enhanced Image");
+d = imadjust(i,[0.2 0.3 0; 0.6 0.7 1],[]);
+subplot(4,2,4);imshow(d);title("Enhanced Image 2");
+
+subplot(4,2,7);imhist(g);title("Histogram of gray image");
+m= histeq(g);
+subplot(4,2,6);imshow(m);title("Equalized Image");
+subplot(4,2,8);imhist(m);title("Histogram of equalized image");
+
+
