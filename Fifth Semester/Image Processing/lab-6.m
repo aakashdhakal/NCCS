bbdfe26b2847a142964f85385487fed31674761f
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-07-12 14:24:00 +0545

    Create lab-6.m

---
+++ Fifth Semester/Image Processing/lab-6.m
@@ -0,0 +1,14 @@
+clear all;
+close all;
+clc;
+
+pkg load image;
+
+i=imread("Batman.jpg");
+i = i(:,:,1); subplot(2,2,1); imshow(i); title("Original Image");
+a = [0.001 0.001 0.001 0.001 0.001 0.001 0.001 0.001 0.001 ];
+R = conv2(a,i);
+ subplot(2,2,2); imshow(R); title("Filtered Image");
+ b = [0.005 0.005 0.005 0.005 0.005 0.005 0.005 0.005 0.005 ];
+R = conv2(b,i);
+ subplot(2,2,3); imshow(R); title("Filtered Image 2");
