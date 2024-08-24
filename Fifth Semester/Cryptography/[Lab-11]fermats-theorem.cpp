b14de9893d17a11483bd8e79f65475c19510ca3a
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-08-24 23:11:47 +0545

    chore: Refactor cryptography code for improved readability and maintainability

--- Fifth Semester/Cryptography/[Lab-11]fermats-theorem.cpp
+++ Fifth Semester/Cryptography/[Lab-11]fermats-theorem.cpp
@@ -1,35 +1,31 @@
-#include <iostream>
 #include <cstdlib>
-#include <algorithm>
+#include <iostream>
 using namespace std;
 
-int power(int x, unsigned int y, unsigned int m);
-void modInverse(int a, int m)
-{
-    if (__gcd(a, m) != 1)
-        cout << "Inverse doesn't exist" << endl;
-    else
-    {
-        cout << "Modular multiplicative inverse is "
-             << power(a, m - 2, m) << endl;
-    }
-}
-int power(int x, unsigned int y, unsigned int m)
-{
-    if (y == 0)
-        return 1;
-    int p = power(x, y / 2, m) % m;
-    p = (p * p) % m;
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
 
-    return (y % 2 == 0) ? p : (x * p) % m;
+int power(int x, unsigned int y, unsigned int m);
+void modInverse(int a, int m) {
+  if (gcd(a, m) != 1)
+    cout << "Inverse doesn't exist" << endl;
+  else {
+    cout << "Modular multiplicative inverse is " << power(a, m - 2, m) << endl;
+  }
 }
-int main()
-{
-    int a, p;
-    cout << "Enter a integer: ";
-    cin >> a;
-    cout << "Enter a prime modular divisor: ";
-    cin >> p;
-    modInverse(a, p);
-    return 0;
+int power(int x, unsigned int y, unsigned int m) {
+  if (y == 0)
+    return 1;
+  int p = power(x, y / 2, m) % m;
+  p = (p * p) % m;
+
+  return (y % 2 == 0) ? p : (x * p) % m;
+}
+int main() {
+  int a, p;
+  cout << "Enter a integer: ";
+  cin >> a;
+  cout << "Enter a prime modular divisor: ";
+  cin >> p;
+  modInverse(a, p);
+  return 0;
 }
--- Fifth Semester/Cryptography/[Lab-9]miller-rabin-test.cpp
+++ Fifth Semester/Cryptography/[Lab-9]miller-rabin-test.cpp
@@ -1,67 +1,54 @@
 #include <iostream>
-#include <cstdlib>
-
 using namespace std;
 
-unsigned long long power(unsigned long long x, unsigned long long y, unsigned long long p)
-{
-    unsigned long long res = 1;
-    x = x % p;
-    while (y > 0)
-    {
-        if (y & 1)
-            res = (res * x) % p;
-        y = y >> 1;
-        x = (x * x) % p;
-    }
-    return res;
+unsigned long long power(unsigned long long x, unsigned long long y,
+                         unsigned long long p) {
+  unsigned long long res = 1;
+  x %= p;
+  while (y > 0) {
+    if (y & 1)
+      res = (res * x) % p;
+    y >>= 1;
+    x = (x * x) % p;
+  }
+  return res;
 }
 
-bool millerTest(unsigned long long d, unsigned long long n)
-{
-    unsigned long long a = 2 + rand() % (n - 4);
-    unsigned long long x = power(a, d, n);
-    if (x == 1 || x == n - 1)
-        return true;
-    while (d != n - 1)
-    {
-        x = (x * x) % n;
-        d *= 2;
-        if (x == 1)
-            return false;
-        if (x == n - 1)
-            return true;
-    }
-    return false;
-}
-
-bool isPrime(unsigned long long n, int k)
-{
-    if (n <= 1 || n == 4)
-        return false;
-    if (n <= 3)
-        return true;
-    unsigned long long d = n - 1;
-    while (d % 2 == 0)
-        d /= 2;
-    for (int i = 0; i < k; i++)
-        if (!millerTest(d, n))
-            return false;
+bool millerTest(unsigned long long d, unsigned long long n) {
+  unsigned long long a = 2 + rand() % (n - 4);
+  unsigned long long x = power(a, d, n);
+  if (x == 1 || x == n - 1)
     return true;
+  while (d != n - 1) {
+    x = (x * x) % n;
+    d *= 2;
+    if (x == 1)
+      return false;
+    if (x == n - 1)
+      return true;
+  }
+  return false;
 }
 
-int main()
-{
-    unsigned long long n;
-    cout << "Enter a number: ";
-    cin >> n;
-    int k = 4;
+bool isPrime(unsigned long long n, int k) {
+  if (n <= 1 || n == 4)
+    return false;
+  if (n <= 3)
+    return true;
+  unsigned long long d = n - 1;
+  while (d % 2 == 0)
+    d /= 2;
+  for (int i = 0; i < k; i++)
+    if (!millerTest(d, n))
+      return false;
+  return true;
+}
 
-    if (isPrime(n, k))
-        cout
-            << n << " is prime" << endl;
-    else
-        cout << n << " is not prime" << endl;
-
-    return 0;
+int main() {
+  unsigned long long n;
+  cout << "Enter a number: ";
+  cin >> n;
+  int k = 4;
+  cout << n << " is " << (isPrime(n, k) ? "Prime" : "Not Prime") << endl;
+  return 0;
 }
